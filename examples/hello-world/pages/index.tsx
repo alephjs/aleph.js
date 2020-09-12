4ad9dfb20678cba319d4fdbca4db17c1a94afa67
Je <git@iamje.com>
2020-09-12 20:40:49 +0800

    chore: update hello example

--- examples/hello-world/pages/index.tsx
+++ examples/hello-world/pages/index.tsx
@@ -2,17 +2,13 @@ import React, { useState } from 'https://esm.sh/react'
 import Logo from '../components/logo.tsx'
 import '../style/index.less'
 
-export async function getStaticProps() {
-    return { name: 'Aleph.js' }
-}
-
-export default function Home({ name }: { name: string }) {
+export default function Home( ) {
     const [count, setCount] = useState(0)
 
     return (
         <div className="wrapper">
             <p className="logo"><Logo /></p>
-            <p>Welcome to use <strong>{name}</strong>!</p>
+            <p>Welcome to use <strong>Aleph.js</strong>!</p>
             <p className="links">
                 <a href="https://alephjs.org/guides" target="_blank">Get Started</a>
                 <span>&middot;</span>
