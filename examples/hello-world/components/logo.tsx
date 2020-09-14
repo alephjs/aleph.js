7ab89e30c61a545069bb0fb80a6778825dd93e25
Je <git@iamje.com>
2020-09-14 16:27:39 +0800

    chore: update logo

--- examples/hello-world/components/logo.tsx
+++ examples/hello-world/components/logo.tsx
@@ -1,6 +1,6 @@
 import React from 'https://esm.sh/react'
 
-export default function Logo({ height = 60 }: { height?: number }) {
+export default function Logo({ height = 66 }: { height?: number }) {
     return (
         <img src="/logo.png" height={height} title="AlephJS" />
     )
