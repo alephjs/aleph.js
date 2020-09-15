c08c739eaf63039116b79488f7f2dc8c812fd21a
Je <git@iamje.com>
2020-09-16 07:25:44 +0800

    refactor: add ErrorPage export

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from './app.ts'
+export { ErrorPage } from './error.ts'
 export { default as Head } from './head.ts'
 export { default as Link } from './link.ts'
 export * from './router.ts'
