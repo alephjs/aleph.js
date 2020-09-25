e4affb2b272cf2b6a8baf9e4c2bb10055569fa0c
Je <git@iamje.com>
2020-09-25 15:16:09 +0800

    chore: remove react-dom-server vendor

--- renderer.ts
+++ renderer.ts
@@ -1,10 +1,10 @@
 import React, { ComponentType } from 'https://esm.sh/react'
+import { renderToString } from 'https://esm.sh/react-dom/server'
 import { DataContext } from './data.ts'
 import { E501 } from './error.ts'
 import { RouterContext } from './router.ts'
 import type { RouterURL } from './types.ts'
 import util from './util.ts'
-import ReactDomServer from './vendor/react-dom-server/server.js'
 export { renderHead } from './head.ts'
 
 export function renderPage(
@@ -15,7 +15,7 @@ export function renderPage(
 ) {
     const pageEl = React.createElement(util.isLikelyReactComponent(Page) ? Page : E501.Page)
     const appEl = App ? (util.isLikelyReactComponent(App) ? React.createElement(App, null, pageEl) : React.createElement(E501.App)) : pageEl
-    return ReactDomServer.renderToString(React.createElement(
+    return renderToString(React.createElement(
         DataContext.Provider,
         { value: data },
         React.createElement(
