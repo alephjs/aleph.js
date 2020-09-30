f189d189a3108a9cd44e260d19639a44f35ada35
Je <git@iamje.com>
2020-10-01 01:44:18 +0800

    refactor: export Seo and Viewport components

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 export { AppManifestContext } from './app.ts'
 export * from './data.ts'
 export { ErrorPage } from './error.ts'
-export { default as Head } from './head.ts'
+export { default as Head, SEO, Viewport } from './head.ts'
 export { default as Link } from './link.ts'
 export * from './router.ts'
 
