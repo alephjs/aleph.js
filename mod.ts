ffb3498311a41869fb8f3df67915df77eb40d7fb
Je <git@iamje.com>
2020-10-05 02:26:02 +0800

    refactor: update package exports

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 export { redirect } from './app.ts'
 export * from './context.ts'
-export { ErrorPage } from './error.ts'
 export { default as Head, SEO, Viewport } from './head.ts'
 export * from './hooks.ts'
 export { default as Link } from './link.ts'
