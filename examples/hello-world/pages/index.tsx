1588f2e4ebc71f8e259fe98db4d57a7cf1d78a66
Je <git@iamje.com>
2020-10-14 17:06:19 +0800

    chore: update hello-world example

--- examples/hello-world/pages/index.tsx
+++ examples/hello-world/pages/index.tsx
@@ -11,6 +11,8 @@ export default function Home() {
             <p className="logo"><Logo /></p>
             <p>Welcome to use <strong>Aleph.js</strong>!</p>
             <p className="links">
+                <a href="https://alephjs.org" target="_blank">Website</a>
+                <span>&middot;</span>
                 <a href="https://alephjs.org/docs/get-started" target="_blank">Get Started</a>
                 <span>&middot;</span>
                 <a href="https://alephjs.org/docs" target="_blank">Docs</a>
