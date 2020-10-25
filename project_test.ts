3ef285cb2fc4282c8495871a0e72908445a56055
Je <git@iamje.com>
2020-10-26 03:51:31 +0800

    chore: update testings

--- project_test.ts
+++ project_test.ts
@@ -2,21 +2,26 @@ import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 import { Project } from './project.ts';
 import { path } from './std.ts';
 
-Deno.test('project build(hello world)', async () => {
-    const output: string[] = []
-    const appDir = path.resolve('./examples/hello-world')
-    const project = new Project(appDir, 'production')
-    await project.build()
-    for await (const entry of Deno.readDir(path.resolve(appDir, 'dist'))) {
-        output.push(entry.name)
-    }
-    output.sort()
-    assertEquals(output, [
-        '404.html',
-        '_aleph',
-        '_fallback.html',
-        'favicon.ico',
-        'index.html',
-        'logo.svg'
-    ])
+Deno.test({
+    name: 'project build(hello world)',
+    async fn() {
+        const output: string[] = []
+        const appDir = path.resolve('./examples/hello-world')
+        const project = new Project(appDir, 'production')
+        await project.build()
+        for await (const entry of Deno.readDir(path.resolve(appDir, 'dist'))) {
+            output.push(entry.name)
+        }
+        output.sort()
+        assertEquals(output, [
+            '404.html',
+            '_aleph',
+            '_fallback.html',
+            'favicon.ico',
+            'index.html',
+            'logo.svg'
+        ])
+    },
+    sanitizeResources: false,
+    sanitizeOps: false
 })
