3e1686565be2ecfeec6cb46276f16e402629832d
Je <git@iamje.com>
2020-10-28 10:32:25 +0800

    chore: rename

--- examples/wasm-loader-plugin/aleph.config.ts
+++
@@ -1,6 +0,0 @@
-import sass from '../../plugins/sass.ts'
-import wasm from '../../plugins/wasm.ts'
-
-export default {
-    plugins: [sass, wasm]
-}
---
+++ examples/with-plugins/aleph.config.ts
@@ -0,0 +1,6 @@
+import sass from 'https://deno.land/x/aleph/plugins/sass.ts'
+import wasm from 'https://deno.land/x/aleph/plugins/wasm.ts'
+
+export default {
+    plugins: [sass, wasm]
+}
