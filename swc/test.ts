40063275ca894e50514ed98b9f4c1daaf64cb7dc
Je <git@iamje.com>
2020-10-31 05:19:09 +0800

    chore(swc): update test.ts

--- swc/test.ts
+++ swc/test.ts
@@ -1,6 +1,33 @@
+import * as colors from 'https://deno.land/std@0.75.0/fmt/colors.ts'
 import init, { transformSync } from './pkg/aleph_swc.js'
 
 const wasmCode = Deno.readFileSync('./pkg/aleph_swc_bg.wasm')
 await init(wasmCode)
-const out = transformSync('const n: number = 123', { filename: './test.ts' })
-console.log(out)
+
+function test(srouce: string, opts: any) {
+    const { code } = transformSync(srouce, opts)
+    console.log(colors.dim(`[${opts.filename}]`))
+    console.log(code.trim() + '\n')
+}
+
+test(`
+const n: number = 123
+if (true) { console.log(n) }
+`, { filename: './test.ts' })
+
+test(`
+const n: number = 123
+if (true) { console.log(n) }
+`, { filename: './test.ts', config: { minify: true } })
+
+test(`
+<div>Hello World!</div>
+`, { filename: './test.jsx' })
+
+test(`
+<div className="title">Hello World!</div>
+`, { filename: './test.jsx', config: { minify: true } })
+
+test(`
+<Import from="./foo.ts" />
+`, { filename: './test.jsx' })
