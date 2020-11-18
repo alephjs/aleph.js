77571e3fab2fee39486ad8c455614619f5232d98
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-18 12:17:19 -0800

    feat: you can also customize rendering settings

--- plugins/sass.ts
+++ plugins/sass.ts
@@ -1,6 +1,6 @@
-import { renderSync } from 'https://esm.sh/sass@1.29.0'
+import { Options, renderSync } from 'https://esm.sh/sass@1.29.0'
 
-export default {
+const defaultOptions = {
     name: 'sass-loader',
     test: /.(sass|scss)$/,
     acceptHMR: true,
@@ -31,3 +31,41 @@ export default {
         }
     }
 }
+
+let plugin: any = (opts: Options) => ({
+    name: 'sass-loader',
+    test: /.(sass|scss)$/,
+    acceptHMR: true,
+    transform(content: Uint8Array, path: string) {
+        if (path.endsWith('.sass')) {
+            const ret = renderSync({
+                ...opts,
+                file: path,
+                data: (new TextDecoder).decode(content),
+                sourceMap: true,
+                indentedSyntax: true
+            })
+            return {
+                code: (new TextDecoder).decode(ret.css),
+                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
+                loader: 'css'
+            }
+        } else {
+            const ret = renderSync({
+                ...opts,
+                file: path,
+                data: (new TextDecoder).decode(content),
+                sourceMap: true
+            })
+            return {
+                code: (new TextDecoder).decode(ret.css),
+                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
+                loader: 'css'
+            }
+        }
+    }
+})
+
+plugin = { ...plugin, ...defaultOptions }
+
+export default plugin;
