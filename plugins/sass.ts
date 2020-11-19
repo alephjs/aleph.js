4610fae1991ccd3a41cc913c2f8eb3aa7c271bb1
X <git@iamje.com>
2020-11-19 12:14:07 +0800

    refactor: simplify

--- plugins/sass.ts
+++ plugins/sass.ts
@@ -1,71 +1,42 @@
 import { Options, renderSync } from 'https://esm.sh/sass@1.29.0'
 
-const defaultOptions = {
+const defaultPlugin = {
     name: 'sass-loader',
     test: /.(sass|scss)$/,
     acceptHMR: true,
     transform(content: Uint8Array, path: string) {
-        if (path.endsWith('.sass')) {
-            const ret = renderSync({
-                file: path,
-                data: (new TextDecoder).decode(content),
-                sourceMap: true,
-                indentedSyntax: true
-            })
-            return {
-                code: (new TextDecoder).decode(ret.css),
-                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
-                loader: 'css'
-            }
-        } else {
-            const ret = renderSync({
-                file: path,
-                data: (new TextDecoder).decode(content),
-                sourceMap: true
-            })
-            return {
-                code: (new TextDecoder).decode(ret.css),
-                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
-                loader: 'css'
-            }
+        const ret = renderSync({
+            file: path,
+            data: (new TextDecoder).decode(content),
+            sourceMap: true,
+            indentedSyntax: path.endsWith('.sass')
+        })
+        return {
+            code: (new TextDecoder).decode(ret.css),
+            map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
+            loader: 'css'
         }
     }
 }
 
 let plugin: any = (opts: Options) => ({
-    name: 'sass-loader',
-    test: /.(sass|scss)$/,
-    acceptHMR: true,
+    ...defaultPlugin,
     transform(content: Uint8Array, path: string) {
-        if (path.endsWith('.sass')) {
-            const ret = renderSync({
-                ...opts,
-                file: path,
-                data: (new TextDecoder).decode(content),
-                sourceMap: true,
-                indentedSyntax: true
-            })
-            return {
-                code: (new TextDecoder).decode(ret.css),
-                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
-                loader: 'css'
-            }
-        } else {
-            const ret = renderSync({
-                ...opts,
-                file: path,
-                data: (new TextDecoder).decode(content),
-                sourceMap: true
-            })
-            return {
-                code: (new TextDecoder).decode(ret.css),
-                map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
-                loader: 'css'
-            }
+        const ret = renderSync({
+            indentedSyntax: path.endsWith('.sass'),
+            ...opts,
+            file: path,
+            data: (new TextDecoder).decode(content),
+            sourceMap: true
+        })
+        return {
+            code: (new TextDecoder).decode(ret.css),
+            map: ret.map ? (new TextDecoder).decode(ret.map) : undefined,
+            loader: 'css'
         }
     }
 })
 
-plugin = { ...plugin, ...defaultOptions }
+plugin = { ...plugin, ...defaultPlugin }
 
 export default plugin;
