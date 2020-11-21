002f185fc13be34978a914c73aacc17dec2c5f89
ije <git@iamje.com>
2020-11-21 19:45:59 +0800

    refactor(swc): support `React.useEffect`

--- swc/src/fast_refresh.rs
+++ swc/src/fast_refresh.rs
@@ -35,8 +35,8 @@ struct HookCall {
 
 #[derive(Clone, Debug)]
 struct Signature {
+  parent_ident: Ident,
   handle_ident: Ident,
-  persistent_ident: Ident,
   hook_calls: Vec<HookCall>,
 }
 
@@ -45,72 +45,72 @@ impl FastRefreshFold {
     &mut self,
     ident: &Ident,
     block_stmt: &mut BlockStmt,
-  ) -> Option<(Ident, Option<Signature>)> {
-    if is_componentish_name(ident.as_ref()) {
-      let mut hook_calls = Vec::<HookCall>::new();
-      let stmts = &block_stmt.stmts;
-      stmts.into_iter().for_each(|stmt| match stmt {
-        Stmt::Expr(ExprStmt { expr, .. }) => match &**expr {
-          Expr::Call(call) => match self.get_hook_call(None, call) {
+  ) -> (Option<Ident>, Option<Signature>) {
+    let persistent_id = if is_componentish_name(ident.as_ref()) {
+      Some(ident.clone())
+    } else {
+      None
+    };
+    let mut hook_calls = Vec::<HookCall>::new();
+    let stmts = &block_stmt.stmts;
+    stmts.into_iter().for_each(|stmt| match stmt {
+      Stmt::Expr(ExprStmt { expr, .. }) => match &**expr {
+        Expr::Call(call) => match self.get_hook_call(None, call) {
+          Some(hc) => hook_calls.push(hc),
+          _ => {}
+        },
+        _ => {}
+      },
+      Stmt::Decl(Decl::Var(var_decl)) => match var_decl.decls.as_slice() {
+        [VarDeclarator {
+          name,
+          init: Some(init_expr),
+          ..
+        }] => match init_expr.as_ref() {
+          Expr::Call(call) => match self.get_hook_call(Some(name.clone()), call) {
             Some(hc) => hook_calls.push(hc),
             _ => {}
           },
           _ => {}
         },
-        Stmt::Decl(Decl::Var(var_decl)) => match var_decl.decls.as_slice() {
-          [VarDeclarator {
-            name,
-            init: Some(init_expr),
-            ..
-          }] => match init_expr.as_ref() {
-            Expr::Call(call) => match self.get_hook_call(Some(name.clone()), call) {
-              Some(hc) => hook_calls.push(hc),
-              _ => {}
-            },
-            _ => {}
-          },
-          _ => {}
-        },
         _ => {}
-      });
-      if hook_calls.len() > 0 {
-        let mut handle_ident = String::from("_s");
-        self.signature_index = self.signature_index + 1;
-        if self.signature_index > 1 {
-          handle_ident.push_str(self.signature_index.to_string().as_str());
-        };
-        let handle_ident = private_ident!(handle_ident.as_str());
-        block_stmt.stmts.insert(
-          0,
-          Stmt::Expr(ExprStmt {
+      },
+      _ => {}
+    });
+    let signature = if hook_calls.len() > 0 {
+      let mut handle_ident = String::from("_s");
+      self.signature_index = self.signature_index + 1;
+      if self.signature_index > 1 {
+        handle_ident.push_str(self.signature_index.to_string().as_str());
+      };
+      let handle_ident = private_ident!(handle_ident.as_str());
+      block_stmt.stmts.insert(
+        0,
+        Stmt::Expr(ExprStmt {
+          span: DUMMY_SP,
+          expr: Box::new(Expr::Call(CallExpr {
             span: DUMMY_SP,
-            expr: Box::new(Expr::Call(CallExpr {
-              span: DUMMY_SP,
-              callee: ExprOrSuper::Expr(Box::new(Expr::Ident(handle_ident.clone()))),
-              args: vec![],
-              type_args: None,
-            })),
-          }),
-        );
-        return Some((
-          ident.clone(),
-          Some(Signature {
-            handle_ident,
-            persistent_ident: ident.clone(),
-            hook_calls,
-          }),
-        ));
-      }
-      Some((ident.clone(), None))
+            callee: ExprOrSuper::Expr(Box::new(Expr::Ident(handle_ident.clone()))),
+            args: vec![],
+            type_args: None,
+          })),
+        }),
+      );
+      Some(Signature {
+        parent_ident: ident.clone(),
+        handle_ident,
+        hook_calls,
+      })
     } else {
       None
-    }
+    };
+    (persistent_id, signature)
   }
 
   fn get_persistent_fc_from_var_decl(
     &mut self,
     var_decl: &mut VarDecl,
-  ) -> Option<(Ident, Option<Signature>)> {
+  ) -> (Option<Ident>, Option<Signature>) {
     match var_decl.decls.as_mut_slice() {
       // We only handle the case when a single variable is declared
       [VarDeclarator {
@@ -128,9 +128,9 @@ impl FastRefreshFold {
           body: BlockStmtOrExpr::BlockStmt(body),
           ..
         }) => self.get_persistent_fc(ident, body),
-        _ => None,
+        _ => (None, None),
       },
-      _ => None,
+      _ => (None, None),
     }
   }
 
@@ -141,7 +141,9 @@ impl FastRefreshFold {
     };
 
     let callee = match callee {
+      // useState()
       Expr::Ident(id) => Some((None, id)),
+      // React.useState()
       Expr::Member(expr) => match &expr.obj {
         ExprOrSuper::Expr(obj) => match &**obj {
           Expr::Ident(obj) => match &*expr.prop {
@@ -196,12 +198,11 @@ impl Fold for FastRefreshFold {
   fn fold_module_items(&mut self, module_items: Vec<ModuleItem>) -> Vec<ModuleItem> {
     let mut items = Vec::<ModuleItem>::new();
     let mut raw_items = Vec::<ModuleItem>::new();
-    let mut registrations = Vec::<Ident>::new();
-    let mut registration_calls = Vec::<CallExpr>::new();
+    let mut registrations = Vec::<(Ident, Ident)>::new();
     let mut signatures = Vec::<Signature>::new();
 
     for mut item in module_items {
-      let persistent_fc: Option<(Ident, Option<Signature>)> = match &mut item {
+      let (persistent_id, signature) = match &mut item {
         // function Foo() {}
         ModuleItem::Stmt(Stmt::Decl(Decl::Fn(FnDecl {
           ident,
@@ -246,16 +247,12 @@ impl Fold for FastRefreshFold {
           ..
         })) => self.get_persistent_fc_from_var_decl(var_decl),
 
-        _ => None,
+        _ => (None, None),
       };
 
       raw_items.push(item);
 
-      if let Some((persistent_id, signature)) = persistent_fc {
-        if let Some(signature) = signature {
-          signatures.push(signature);
-        }
-
+      if let Some(persistent_id) = persistent_id {
         let mut registration_handle = String::from("_c");
         let registration_index = registrations.len() + 1;
         if registration_index > 1 {
@@ -263,30 +260,7 @@ impl Fold for FastRefreshFold {
         };
         let registration_handle = private_ident!(registration_handle.as_str());
 
-        registrations.push(registration_handle.clone());
-
-        // $RefreshReg$(_c, "App");
-        registration_calls.push(CallExpr {
-          span: DUMMY_SP,
-          callee: ExprOrSuper::Expr(Box::new(Expr::Ident(quote_ident!(self
-            .refresh_reg
-            .as_str())))),
-          args: vec![
-            ExprOrSpread {
-              spread: None,
-              expr: Box::new(Expr::Ident(registration_handle.clone())),
-            },
-            ExprOrSpread {
-              spread: None,
-              expr: Box::new(Expr::Lit(Lit::Str(Str {
-                span: DUMMY_SP,
-                value: persistent_id.sym.clone(),
-                has_escape: false,
-              }))),
-            },
-          ],
-          type_args: None,
-        });
+        registrations.push((registration_handle.clone(), persistent_id.clone()));
 
         raw_items.push(ModuleItem::Stmt(Stmt::Expr(ExprStmt {
           span: DUMMY_SP,
@@ -297,6 +271,10 @@ impl Fold for FastRefreshFold {
             right: Box::new(Expr::Ident(persistent_id)),
           })),
         })));
+      }
+
+      if let Some(signature) = signature {
+        signatures.push(signature);
       }
     }
 
@@ -312,9 +290,9 @@ impl Fold for FastRefreshFold {
         decls: registrations
           .clone()
           .into_iter()
-          .map(|handle| VarDeclarator {
+          .map(|registration| VarDeclarator {
             span: DUMMY_SP,
-            name: Pat::Ident(handle),
+            name: Pat::Ident(registration.0),
             init: None,
             definite: false,
           })
@@ -369,7 +347,7 @@ impl Fold for FastRefreshFold {
           args: vec![
             ExprOrSpread {
               spread: None,
-              expr: Box::new(Expr::Ident(signature.persistent_ident.clone())),
+              expr: Box::new(Expr::Ident(signature.parent_ident.clone())),
             },
             ExprOrSpread {
               spread: None,
@@ -396,13 +374,32 @@ impl Fold for FastRefreshFold {
     // $RefreshReg$(_c, "App");
     // $RefreshReg$(_c2, "Foo");
     // ```
-    for call in registration_calls {
+    for (handle_id, persistent_id) in registrations {
       items.push(ModuleItem::Stmt(Stmt::Expr(ExprStmt {
         span: DUMMY_SP,
-        expr: Box::new(Expr::Call(call)),
+        expr: Box::new(Expr::Call(CallExpr {
+          span: DUMMY_SP,
+          callee: ExprOrSuper::Expr(Box::new(Expr::Ident(quote_ident!(self
+            .refresh_reg
+            .as_str())))),
+          args: vec![
+            ExprOrSpread {
+              spread: None,
+              expr: Box::new(Expr::Ident(handle_id)),
+            },
+            ExprOrSpread {
+              spread: None,
+              expr: Box::new(Expr::Lit(Lit::Str(Str {
+                span: DUMMY_SP,
+                value: persistent_id.sym.clone(),
+                has_escape: false,
+              }))),
+            },
+          ],
+          type_args: None,
+        })),
       })));
     }
-
     items
   }
 }
