3d179a3f88cbb99169fabcd21970a188eddaabf1
Je <git@iamje.com>
2020-11-24 22:04:00 +0800

    chore(swc): add build script

--- swc/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wasm-pack build --target web
---
+++ swc/build.ts
@@ -0,0 +1,28 @@
+import { base64, brotli } from '../deps.ts'
+
+export async function run(...cmd: string[]) {
+    const p = Deno.run({
+        cmd,
+        stdout: 'inherit',
+        stderr: 'inherit'
+    })
+    await p.status()
+    p.close()
+}
+
+async function build() {
+    await run('wasm-pack', 'build', '--target', 'web')
+    const wasmData = await Deno.readFile('./pkg/aleph_swc_bg.wasm')
+    const data = brotli.compress(wasmData)
+    const dataStr = base64.encode(data)
+    await Deno.writeTextFile('./aleph_swc.wasm.js', [
+        `import { base64, brotli } from '../deps.ts';`,
+        `const dataRaw = '${dataStr}';`,
+        `export default () => brotli.decompress(base64.decode(dataRaw))`
+    ].join('\n'))
+    await Deno.copyFile('./pkg/aleph_swc.js', './aleph_swc.js')
+}
+
+if (import.meta.main) {
+    build()
+}
