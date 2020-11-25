354f76c0f8150793b93109f3f533bf01e7cc9e66
Je <git@iamje.com>
2020-11-25 23:34:49 +0800

    refactor(cli): improve upgrade command

--- cli/upgrade.ts
+++ cli/upgrade.ts
@@ -1,4 +1,5 @@
 import { colors } from '../deps.ts'
+import { version as currentVersion } from '../version.ts'
 
 export const helpMessage = `
 Usage:
@@ -12,25 +13,30 @@ Options:
 async function run(...cmd: string[]) {
     const p = Deno.run({
         cmd,
-        stdout: 'piped',
+        stdout: 'null',
         stderr: 'piped'
     })
-    Deno.stdout.write(await p.output())
     Deno.stderr.write(await p.stderrOutput())
     p.close()
 }
 
 export default async function (version: string) {
+    console.log('Looking up latest version...')
     const { latest, versions } = await (await fetch('https://cdn.deno.land/aleph/meta/versions.json')).json()
     if (version === 'latest') {
         version = latest
     } else if (!versions.includes(version)) {
         version = 'v' + version
         if (!versions.includes(version)) {
-            console.log(`${colors.red('error')}: version(${version}) not found`)
+            console.log(`${colors.red('error')}: version(${version}) not found!`)
             Deno.exit(1)
         }
     }
+    if (version === 'v' + currentVersion) {
+        console.log('Already up-to-date!')
+        Deno.exit(0)
+    }
     await run('deno', 'install', '-A', '-f', '-n', 'aleph', `https://deno.land/x/aleph@${version}/cli.ts`)
+    console.log(`Aleph.js is up to ${version}!`)
     Deno.exit(0)
 }
