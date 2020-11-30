503509f5fc6f2aaec763e03e03facb6a0c56c3aa
Je <git@iamje.com>
2020-11-30 19:06:10 +0800

    chore: update examples

--- examples/with-plugins/pages/index.tsx
+++ examples/with-plugins/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // @ts-expect-error
 import wasm from './42.wasm'
-// @ts-expect-error
-import './style.sass'
 
 export default function Home() {
     return (
-        <h1>{wasm.main()}</h1>
+        <>
+            <link rel="stylesheet" href="./style.sass" />
+            <h1>{wasm.main()}</h1>
+        </>
     )
 }
