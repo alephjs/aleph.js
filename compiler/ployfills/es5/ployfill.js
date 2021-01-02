469b00a17e1c5b922ba45f26186f19283e67a223
ije <git@iamje.com>
2021-01-02 18:53:21 +0800

    refactor(compiler): update es5 ployfill

--- compiler/ployfills/es5/ployfill.js
+++ compiler/ployfills/es5/ployfill.js
@@ -1,12 +1,5 @@
 import { mark, wrap } from 'https://esm.sh/regenerator-runtime@0.13.7'
 
-window.require = function (name) {
-  if (name === 'regenerator-runtime') {
-    if ('regeneratorRuntime' in window) {
-      return regeneratorRuntime
-    }
-    return { mark, wrap }
-  } else {
-    throw new Error(`module ${name} undefined`)
-  }
+if (!('regeneratorRuntime' in window)) {
+  window.regeneratorRuntime = { mark, wrap }
 }
