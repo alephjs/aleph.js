07aacbe0b60ceb53cbc550779aeb9d6f1b89adb9
X <i@jex.me>
2021-02-04 03:48:37 +0800

    fix(cli): fix typo: Appliaction -> Application

--- cli/dev.ts
+++ cli/dev.ts
@@ -1,4 +1,5 @@
-import { Appliaction, parsePortNumber, serve } from '../server/mod.ts'
+import { Application, serve } from '../server/mod.ts'
+import { parsePortNumber } from '../server/util.ts'
 
 export const helpMessage = `
 Usage:
@@ -16,6 +17,6 @@ Options:
 
 export default async function (workingDir: string, options: Record<string, string | boolean>) {
     const port = parsePortNumber(String(options.p || options.port || '8080'))
-    const app = new Appliaction(workingDir, 'development', Boolean(options.r || options.reload))
+    const app = new Application(workingDir, 'development', Boolean(options.r || options.reload))
     serve('localhost', port, app)
 }
--- cli/start.ts
+++ cli/start.ts
@@ -1,4 +1,5 @@
-import { Appliaction, parsePortNumber, serve } from '../server/mod.ts'
+import { Application, serve } from '../server/mod.ts'
+import { parsePortNumber } from '../server/util.ts'
 
 export const helpMessage = `
 Usage:
@@ -18,6 +19,6 @@ Options:
 export default async function (workingDir: string, options: Record<string, string | boolean>) {
     const host = String(options.hn || options.hostname || 'localhost')
     const port = parsePortNumber(String(options.p || options.port || '8080'))
-    const app = new Appliaction(workingDir, 'production', Boolean(options.r || options.reload))
+    const app = new Application(workingDir, 'production', Boolean(options.r || options.reload))
     serve(host, port, app)
 }
