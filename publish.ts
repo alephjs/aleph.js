04161b478f6b8b3795cb7d048a7822b131a0e13e
X <i@jex.me>
2021-02-06 18:01:46 +0800

    chore: update publish script

--- publish.ts
+++ publish.ts
@@ -1,9 +1,6 @@
-// custom scripts for deno.land/x/publish
+// custom script for deno.land/x/publish
 
 export async function prepublish(version: string) {
-    // update the version.ts before re-build the compiler wasm.
-    Deno.writeTextFile('./version.ts', `export const VERSION = '${version}'\n`)
-
     const p = Deno.run({
         cmd: ['deno', 'run', '-A', 'build.ts'],
         cwd: './compiler',
