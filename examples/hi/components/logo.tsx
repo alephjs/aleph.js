92f085dd125ed0c5e73c4a94bf443595094fcea9
X <i@jex.me>
2021-02-08 04:03:42 +0800

    chore: rename examples

--- examples/hi/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Logo({ size = 75 }: { size?: number }) {
-  return (
-    <p className="logo">
-      <img src="/logo.svg" height={size} title="Aleph.js" />
-    </p>
-  )
-}
