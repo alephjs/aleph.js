fb922481a3a84617d31c36afea50e4e28dfc68e6
X <i@jex.me>
2021-02-20 18:33:28 +0800

    chore: bump std version to 0.88.0

--- framework/core/routing_test.ts
+++ framework/core/routing_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.86.0/testing/asserts.ts'
+import { assertEquals } from 'https://deno.land/std@0.88.0/testing/asserts.ts'
 import { Routing } from './routing.ts'
 
 Deno.test(`routing`, () => {
