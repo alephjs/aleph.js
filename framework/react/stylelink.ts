4e9c9f6f7ae89b0bf202240ee75de51e72cd7e38
X <i@jex.me>
2021-02-20 21:57:57 +0800

    fix(fw/react): fix `StyleLink` component

--- framework/react/stylelink.ts
+++ framework/react/stylelink.ts
@@ -1,13 +1,10 @@
 import { useEffect } from 'https://esm.sh/react'
-import util from '../../shared/util.ts'
 import { recoverCSS, removeCSS } from '../core/style.ts'
 
 export default function StyleLink({ href }: { href: string }) {
-  useEffect(() => () => {
+  useEffect(() => {
     recoverCSS(href)
-    if (util.isNEString(href)) {
-      removeCSS(href, true)
-    }
+    return () => removeCSS(href, true)
   }, [])
 
   return null
