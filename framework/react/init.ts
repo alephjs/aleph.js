6050f8377d415177cdee6ea139ae4519ae5acd97
X <i@jex.me>
2021-02-20 18:18:00 +0800

    feat(fw/react): add injects in dev mode

--- framework/react/init.ts
+++ framework/react/init.ts
@@ -1,12 +1,47 @@
-import { getAlephPkgUri } from '../../server/helper.ts'
+import { path } from '../../deps.ts'
+import { getAlephPkgUri, getRelativePath, toLocalUrl } from '../../server/helper.ts'
+import util from '../../shared/util.ts'
 import type { ServerApplication } from '../../types.ts'
 
 export async function init(app: ServerApplication) {
-  if (app.mode == 'development') {
+  if (app.mode === 'development') {
+    const alephPkgUri = getAlephPkgUri()
+    app.injectCode('hmr', (url: string, code: string) => {
+      const reactRefresh = code.includes('$RefreshReg$') || code.includes('$RefreshSig$')
+      if (reactRefresh) {
+        const refreshModuleUrl = getRelativePath(
+          path.dirname(toLocalUrl(url)),
+          toLocalUrl(`${alephPkgUri}/framework/react/refresh.js`)
+        )
+        return [
+          `import { RefreshRuntime, performReactRefresh } from ${JSON.stringify(refreshModuleUrl)};`,
+          '',
+          'const prevRefreshReg = window.$RefreshReg$;',
+          'const prevRefreshSig = window.$RefreshSig$;',
+          `window.$RefreshReg$ = (type, id) => RefreshRuntime.register(type, ${JSON.stringify(url)} + "#" + id);`,
+          'window.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;',
+          '',
+          util.trimSuffix(code.trim(), 'import.meta.hot.accept();'),
+          'window.$RefreshReg$ = prevRefreshReg;',
+          'window.$RefreshSig$ = prevRefreshSig;',
+          'import.meta.hot.accept(performReactRefresh);'
+        ].join('\n')
+      }
+      return code
+    })
+    app.injectCode('compilation', (url: string, code: string) => {
+      if (url === '/main.js') {
+        return [
+          `import '.${toLocalUrl(`${alephPkgUri}/framework/react/refresh.js`)}';`,
+          code
+        ].join('\n')
+      }
+      return code
+    })
+    await app.addModule(`${alephPkgUri}/framework/react/refresh.ts`)
     Object.assign(globalThis, {
       $RefreshReg$: () => { },
       $RefreshSig$: () => (type: any) => type,
     })
-    await app.addModule(`${getAlephPkgUri()}/framework/react/refresh.ts`)
   }
 }
