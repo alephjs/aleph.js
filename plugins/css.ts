fa3799446404e494123f09b6834a0e8bf180264e
X <i@jex.me>
2021-03-06 14:38:21 +0800

    refactor: improve css-loader plugin

--- plugins/css.ts
+++ plugins/css.ts
@@ -1,6 +1,5 @@
-import CleanCSS from 'https://esm.sh/clean-css@5.0.1?no-check'
-import type { AcceptedPlugin } from 'https://esm.sh/postcss@8.2.4'
-import PostCSS from 'https://esm.sh/postcss@8.2.4'
+import CleanCSS from 'https://esm.sh/clean-css@5.1.1?no-check'
+import PostCSS, { AcceptedPlugin } from 'https://esm.sh/postcss@8.2.7'
 import { path } from '../deps.ts'
 import { existsFileSync } from '../shared/fs.ts'
 import util from '../shared/util.ts'
@@ -16,35 +15,10 @@ type Options = {
 }
 
 export default (options?: Options): LoaderPlugin => {
-  let postcss: any = null
-  let loading = (async () => {
-    if (options?.postcss) {
-      postcss = PostCSS(await loadPostcssPlugins(options.postcss.plugins))
-      return
-    }
+  const isDev = Deno.env.get('BUILD_MODE') === 'production'
+  const decoder = new TextDecoder()
 
-    for (const name of Array.from(['ts', 'js', 'json']).map(ext => `postcss.config.${ext}`)) {
-      const p = path.join(Deno.cwd(), name)
-      if (existsFileSync(p)) {
-        let config: any = null
-        if (name.endsWith('.json')) {
-          config = JSON.parse(await Deno.readTextFile(p))
-        } else {
-          const mod = await import('file://' + p)
-          config = mod.default
-          if (util.isFunction(config)) {
-            config = await config()
-          }
-        }
-        if (isPostcssConfig(config)) {
-          postcss = PostCSS(await loadPostcssPlugins(config.plugins))
-          return
-        }
-      }
-    }
-
-    postcss = PostCSS(await loadPostcssPlugins(['autoprefixer']))
-  })()
+  let pcssProcessor: any = null
 
   return {
     name: 'css-loader',
@@ -52,11 +26,11 @@ export default (options?: Options): LoaderPlugin => {
     test: /\.p?css$/i,
     acceptHMR: true,
     async transform({ url, content }) {
-      if (postcss == null) {
-        await loading
+      if (pcssProcessor == null) {
+        pcssProcessor = await initPostCSSProcessor(options)
       }
-      const pcss = (await postcss!.process((new TextDecoder).decode(content)).async()).content
-      const css = Deno.env.get('BUILD_MODE') === 'production' ? cleanCSS.minify(pcss).styles : pcss
+      const { content: pcss } = await pcssProcessor.process(decoder.decode(content)).async()
+      const css = isDev ? cleanCSS.minify(pcss).styles : pcss
       if (url.startsWith('#inline-style-')) {
         return {
           code: css,
@@ -75,6 +49,33 @@ export default (options?: Options): LoaderPlugin => {
   }
 }
 
+async function initPostCSSProcessor(options?: Options) {
+  if (options?.postcss) {
+    return PostCSS(await loadPostcssPlugins(options.postcss.plugins))
+  }
+
+  for (const name of Array.from(['ts', 'js', 'json']).map(ext => `postcss.config.${ext}`)) {
+    const p = path.join(Deno.cwd(), name)
+    if (existsFileSync(p)) {
+      let config: any = null
+      if (name.endsWith('.json')) {
+        config = JSON.parse(await Deno.readTextFile(p))
+      } else {
+        const mod = await import('file://' + p)
+        config = mod.default
+        if (util.isFunction(config)) {
+          config = await config()
+        }
+      }
+      if (isPostcssConfig(config)) {
+        return PostCSS(await loadPostcssPlugins(config.plugins))
+      }
+    }
+  }
+
+  return PostCSS(await loadPostcssPlugins(['autoprefixer']))
+}
+
 async function loadPostcssPlugins(plugins: (string | [string, any] | AcceptedPlugin)[]) {
   const isDev = Deno.env.get('BUILD_MODE') === 'development'
   return await Promise.all(plugins.filter(p => {
@@ -88,17 +89,17 @@ async function loadPostcssPlugins(plugins: (string | [string, any] | AcceptedPlu
     return true
   }).map(async p => {
     if (util.isNEString(p)) {
-      return await importPostcssPlugin(p)
+      return await importPostcssPluginByName(p)
     } else if (Array.isArray(p)) {
-      const Plugin = await importPostcssPlugin(p[0])
+      const Plugin = await importPostcssPluginByName(p[0])
       return [Plugin, p[1]]
     } else {
       return p
     }
-  }) || [])
+  }))
 }
 
-async function importPostcssPlugin(name: string) {
+async function importPostcssPluginByName(name: string) {
   const { default: Plugin } = await import(`https://esm.sh/${name}?external=postcss@8.2.4&no-check`)
   return Plugin
 }
