0b57b2634702890d8ba61ae5abf75fdde4eb7757
X <i@jex.me>
2021-03-17 13:36:29 +0800

    breaking(compiler): rewrite CompatFixer fold

--- compiler/src/fixer.rs
+++ compiler/src/fixer.rs
@@ -14,31 +14,42 @@ struct CompatFixer {}
 impl Fold for CompatFixer {
   noop_fold_type!();
 
-  // - `require("regenerator-runtime")` -> `__ALEPH.require("regenerator-runtime")`
+  // - `require("regenerator-runtime")` -> `(() => regeneratorRuntime)()`
   fn fold_call_expr(&mut self, call: CallExpr) -> CallExpr {
     if is_call_expr_by_name(&call, "require") {
-      let ok = match call.args.first() {
+      let name = match call.args.first() {
         Some(ExprOrSpread { expr, .. }) => match expr.as_ref() {
           Expr::Lit(lit) => match lit {
-            Lit::Str(_) => true,
-            _ => false,
+            Lit::Str(s) => s.value.as_ref(),
+            _ => "",
           },
-          _ => false,
+          _ => "",
         },
-        _ => false,
+        _ => "",
       };
-      if ok {
-        return CallExpr {
-          span: DUMMY_SP,
-          callee: ExprOrSuper::Expr(Box::new(Expr::Member(MemberExpr {
+      match name {
+        "regenerator-runtime" => {
+          return CallExpr {
             span: DUMMY_SP,
-            obj: ExprOrSuper::Expr(Box::new(Expr::Ident(quote_ident!("__ALEPH")))),
-            prop: Box::new(Expr::Ident(quote_ident!("require"))),
-            computed: false,
-          }))),
-          args: call.args,
-          type_args: None,
-        };
+            callee: ExprOrSuper::Expr(Box::new(Expr::Paren(ParenExpr {
+              span: DUMMY_SP,
+              expr: Box::new(Expr::Arrow(ArrowExpr {
+                span: DUMMY_SP,
+                params: vec![],
+                body: BlockStmtOrExpr::Expr(Box::new(Expr::Ident(quote_ident!(
+                  "regeneratorRuntime"
+                )))),
+                is_async: false,
+                is_generator: false,
+                type_params: None,
+                return_type: None,
+              })),
+            }))),
+            args: vec![],
+            type_args: None,
+          };
+        }
+        _ => {}
       }
     }
     call
