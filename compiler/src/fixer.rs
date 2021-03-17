e013cbc349598bd7be40615be3ff1f3b2ae9aefb
X <i@jex.me>
2021-03-17 21:22:49 +0800

    refactor(compiler): add tesing for CompatFixer

--- compiler/src/fixer.rs
+++ compiler/src/fixer.rs
@@ -14,7 +14,7 @@ struct CompatFixer {}
 impl Fold for CompatFixer {
   noop_fold_type!();
 
-  // - `require("regenerator-runtime")` -> `(() => regeneratorRuntime)()`
+  // - `require("regenerator-runtime")` -> `(() => window.regeneratorRuntime)()`
   fn fold_call_expr(&mut self, call: CallExpr) -> CallExpr {
     if is_call_expr_by_name(&call, "require") {
       let name = match call.args.first() {
@@ -36,9 +36,10 @@ impl Fold for CompatFixer {
               expr: Box::new(Expr::Arrow(ArrowExpr {
                 span: DUMMY_SP,
                 params: vec![],
-                body: BlockStmtOrExpr::Expr(Box::new(Expr::Ident(quote_ident!(
-                  "regeneratorRuntime"
-                )))),
+                body: BlockStmtOrExpr::Expr(Box::new(Expr::MetaProp(MetaPropExpr {
+                  meta: quote_ident!("window"),
+                  prop: quote_ident!("regeneratorRuntime"),
+                }))),
                 is_async: false,
                 is_generator: false,
                 type_params: None,
@@ -53,5 +54,51 @@ impl Fold for CompatFixer {
       }
     }
     call
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::swc::ParsedModule;
+  use std::cmp::min;
+  use swc_common::Globals;
+
+  fn t(specifier: &str, source: &str, expect: &str) -> bool {
+    let module = ParsedModule::parse(specifier, source, None).expect("could not parse module");
+    let (code, _) = swc_common::GLOBALS.set(&Globals::new(), || {
+      module
+        .apply_transform(compat_fixer_fold(), false)
+        .expect("could not transpile module")
+    });
+
+    if code != expect {
+      let mut p: usize = 0;
+      for i in 0..min(code.len(), expect.len()) {
+        if code.get(i..i + 1) != expect.get(i..i + 1) {
+          p = i;
+          break;
+        }
+      }
+      println!(
+        "{}\x1b[0;31m{}\x1b[0m",
+        code.get(0..p).unwrap(),
+        code.get(p..).unwrap()
+      );
+    }
+    code == expect
+  }
+
+  #[test]
+  fn fast_refresh() {
+    let source = r#"require("regenerator-runtime")
+const { mark } = require("regenerator-runtime")
+    "#;
+    let expect = r#"(()=>window.regeneratorRuntime
+)();
+const { mark  } = (()=>window.regeneratorRuntime
+)();
+"#;
+    assert!(t("/app.js", source, expect));
   }
 }
