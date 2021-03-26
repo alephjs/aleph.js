2ae45401edb3651701824f7ecfb340886195c128
X <i@jex.me>
2021-03-27 03:52:25 +0800

    chore: fix hello-world-spa example config

--- examples/hello-world-spa/aleph.config.ts
+++ examples/hello-world-spa/aleph.config.ts
@@ -2,6 +2,7 @@ import css from '../../plugins/css.ts'
 import type { Config } from '../../types.ts'
 
 export default (): Config => ({
+  ssr: false,
   plugins: [
     css({
       postcss: { plugins: ['autoprefixer'] }
