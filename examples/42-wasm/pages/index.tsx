228696d58c726c5064f8657a9439e49e8b01c11f
X <i@jex.me>
2021-04-04 21:07:41 +0800

    Update the 42-wasm example

--- examples/42-wasm/pages/index.tsx
+++ examples/42-wasm/pages/index.tsx
@@ -2,11 +2,26 @@ import React from 'react'
 // @ts-expect-error
 import wasm from '../lib/42.wasm'
 
+const fontSize = 240
+
 export default function Home() {
   return (
-    <>
-      <link rel="stylesheet" href="../style/style.scss" />
+    <main>
+      <style>{`
+        body {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100vw;
+          height: 100vh;
+          overflow: hidden;
+        }
+
+        h1 {
+          font-size: ${fontSize}px;
+        }
+      `}</style>
       <h1>{wasm.main()}</h1>
-    </>
+    </main>
   )
 }
