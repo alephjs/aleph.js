d877605fe39887c54608d551d9e38840dba7e77c
getspooky <getspookydev@gmail.com>
2021-04-14 14:34:12 +0000

    feat(server): Add acceptsEncodings function

--- server/api.ts
+++ server/api.ts
@@ -1,47 +1,54 @@
-import { Status } from 'https://deno.land/std@0.92.0/http/http_status.ts'
-import type { BufReader, BufWriter } from 'https://deno.land/std@0.92.0/io/bufio.ts'
-import type { MultipartFormData } from 'https://deno.land/std@0.92.0/mime/multipart.ts'
-import { MultipartReader } from 'https://deno.land/std@0.92.0/mime/multipart.ts'
-import log from '../shared/log.ts'
-import type { APIRequest, ServerRequest, ServerResponse } from '../types.ts'
-import { preferredLanguages } from './negotiation.ts'
+import { Status } from "https://deno.land/std@0.92.0/http/http_status.ts"
+import type {
+  BufReader,
+  BufWriter
+} from "https://deno.land/std@0.92.0/io/bufio.ts"
+import type { MultipartFormData } from "https://deno.land/std@0.92.0/mime/multipart.ts"
+import { MultipartReader } from "https://deno.land/std@0.92.0/mime/multipart.ts"
+import log from "../shared/log.ts"
+import type { APIRequest, ServerRequest, ServerResponse } from "../types.ts"
+import { preferredEncodings } from "./negotiation.ts"
 
 let brotli: ((data: Uint8Array) => Uint8Array) | null = null
-let gzip: ((data: Uint8Array) => Uint8Array) | null = null
+let gzip: ((data: Uint8Array) => Uint8Array) | null = null;
 
 export class Request implements APIRequest {
   #req: ServerRequest
   #params: Record<string, string>
   #query: URLSearchParams
-  #cookies: ReadonlyMap<string, string>
+  #cookies: ReadonlyMap<string, string>;
   #resp = {
     status: 200,
     headers: new Headers({
-      Server: 'Aleph.js',
+      Server: "Aleph.js",
     }),
-    done: false
-  }
+    done: false,
+  };
 
-  constructor(req: ServerRequest, params: Record<string, string>, query: URLSearchParams) {
+  constructor(
+    req: ServerRequest,
+    params: Record<string, string>,
+    query: URLSearchParams,
+  ) {
     this.#req = req
     this.#params = params
     this.#query = query
     const cookies = new Map()
-    this.headers.get('cookie')?.split(';').forEach(cookie => {
-      const p = cookie.trim().split('=')
+    this.headers.get("cookie")?.split(";").forEach((cookie) => {
+      const p = cookie.trim().split("=");
       if (p.length >= 2) {
-        cookies.set(p.shift()!.trim(), decodeURI(p.join('=')))
+        cookies.set(p.shift()!.trim(), decodeURI(p.join("=")));
       }
     })
     this.#cookies = cookies
   }
 
   get url(): string {
-    return this.#req.url
+    return this.#req.url;
   }
 
   get method(): string {
-    return this.#req.method
+    return this.#req.method;
   }
 
   get hostname(): string {
@@ -49,181 +56,199 @@ export class Request implements APIRequest {
   }
 
   get headers(): Headers {
-    return this.#req.headers
+    return this.#req.headers;
   }
 
   get conn(): Deno.Conn {
-    return this.#req.conn
+    return this.#req.conn;
   }
 
   get r(): BufReader {
-    return this.#req.r
+    return this.#req.r;
   }
 
   get w(): BufWriter {
-    return this.#req.w
+    return this.#req.w;
   }
 
   get body(): Deno.Reader {
-    return this.#req.body
+    return this.#req.body;
   }
 
   async respond(r: ServerResponse): Promise<void> {
-    return this.#req.respond(r)
+    return this.#req.respond(r);
   }
 
   get params(): Record<string, string> {
-    return this.#params
+    return this.#params;
   }
 
   get query(): URLSearchParams {
-    return this.#query
+    return this.#query;
   }
 
   get cookies(): ReadonlyMap<string, string> {
-    return this.#cookies
+    return this.#cookies;
   }
 
-  async readBody(type?: 'raw'): Promise<Uint8Array>
-  async readBody(type: 'text'): Promise<string>
-  async readBody(type: 'json'): Promise<any>
-  async readBody(type: 'form'): Promise<MultipartFormData>
+  async readBody(type?: "raw"): Promise<Uint8Array>
+  async readBody(type: "text"): Promise<string>
+  async readBody(type: "json"): Promise<any>
+  async readBody(type: "form"): Promise<MultipartFormData>;
   async readBody(type?: string): Promise<any> {
     switch (type) {
-      case 'text': {
+      case "text": {
         const buff: Uint8Array = await Deno.readAll(this.body)
-        const encoded = new TextDecoder('utf-8').decode(buff)
-        return encoded
+        const encoded = new TextDecoder("utf-8").decode(buff)
+        return encoded;
       }
-      case 'json': {
+      case "json": {
         const buff: Uint8Array = await Deno.readAll(this.body)
-        const encoded = new TextDecoder('utf-8').decode(buff)
+        const encoded = new TextDecoder("utf-8").decode(buff)
         const data = JSON.parse(encoded)
-        return data
+        return data;
       }
-      case 'form': {
-        const contentType = this.headers.get('content-type') as string
+      case "form": {
+        const contentType = this.headers.get("content-type") as string
         const reader = new MultipartReader(this.body, contentType)
-        return reader.readForm()
+        return reader.readForm();
       }
       default: {
-        return await Deno.readAll(this.body)
+        return await Deno.readAll(this.body);
       }
     }
   }
 
   addHeader(key: string, value: string): this {
     this.#resp.headers.append(key, value)
-    return this
+    return this;
   }
 
   setHeader(key: string, value: string): this {
     this.#resp.headers.set(key, value)
-    return this
+    return this;
   }
 
   removeHeader(key: string): this {
     this.#resp.headers.delete(key)
-    return this
+    return this;
   }
 
   status(code: number): this {
     this.#resp.status = code
-    return this
+    return this;
   }
 
   redirect(url: string, status: Status = Status.Found): this {
     // "back" is an alias for the referrer.
     if (url == "back") {
-      url = this.#resp.headers.get("Referrer") || "/"
+      url = this.#resp.headers.get("Referrer") || "/";
     }
     this.#resp.status = status
     this.#resp.headers.set("Location", encodeURI(url))
-    return this
+    return this;
   }
 
-  acceptsLanguages(): string[] | undefined
-  acceptsLanguages(...langs: string[]): string[] | string | undefined {
-    const acceptLanguageValue = this.#req.headers.get(
-      "Accept-Language",
+  acceptsEncodings(...encodings: string[]): string | undefined
+  acceptsEncodings(...encodings: string[]): string[] | string | undefined {
+    const acceptEncodingValue = this.#req.headers.get(
+      "Accept-Encoding",
     )
-    if (!acceptLanguageValue) {
-      return
+    if (!acceptEncodingValue) {
+      return;
     }
-    if (langs.length) {
-      return preferredLanguages(acceptLanguageValue, langs)[0]
+    if (encodings.length) {
+      return preferredEncodings(acceptEncodingValue, encodings)[0];
     }
-    return preferredLanguages(acceptLanguageValue)
+    return preferredEncodings(acceptEncodingValue);
   }
 
-  async send(data?: string | Uint8Array | ArrayBuffer, contentType?: string): Promise<void> {
+  async send(
+    data?: string | Uint8Array | ArrayBuffer,
+    contentType?: string,
+  ): Promise<void> {
     if (this.#resp.done) {
-      log.warn('ServerRequest: repeat respond calls')
-      return Promise.resolve()
+      log.warn("ServerRequest: repeat respond calls")
+      return Promise.resolve();
     }
 
     let body = new Uint8Array()
-    if (typeof data === 'string') {
-      body = new TextEncoder().encode(data)
+    if (typeof data === "string") {
+      body = new TextEncoder().encode(data);
     } else if (data instanceof Uint8Array) {
-      body = data
+      body = data;
     } else if (data instanceof ArrayBuffer) {
-      body = new Uint8Array(data)
+      body = new Uint8Array(data);
     }
     if (contentType) {
-      this.#resp.headers.set('Content-Type', contentType)
-    } else if (this.#resp.headers.has('Content-Type')) {
-      contentType = this.#resp.headers.get('Content-Type')!
-    } else if (typeof data === 'string' && data.length > 0) {
-      contentType = 'text/plain; charset=utf-8'
-      this.#resp.headers.set('Content-Type', contentType)
+      this.#resp.headers.set("Content-Type", contentType)
+    } else if (this.#resp.headers.has("Content-Type")) {
+      contentType = this.#resp.headers.get("Content-Type")!
+    } else if (typeof data === "string" && data.length > 0) {
+      contentType = "text/plain; charset=utf-8"
+      this.#resp.headers.set("Content-Type", contentType);
     }
-    if (Deno.env.get('ALEPH_BUILD_MODE') !== 'development') {
-      let shouldCompress = false
+    if (Deno.env.get("ALEPH_BUILD_MODE") !== "development") {
+      let shouldCompress = false;
       if (contentType) {
-        if (contentType.startsWith('text/')) {
+        if (contentType.startsWith("text/")) {
           shouldCompress = true
-        } else if (/^application\/(javascript|typecript|wasm|json|xml)/i.test(contentType)) {
-          shouldCompress = true
+        } else if (
+          /^application\/(javascript|typecript|wasm|json|xml)/i.test(
+            contentType,
+          )
+        ) {
+          shouldCompress = true;
         } else if (/^image\/svg\+xml/i.test(contentType)) {
-          shouldCompress = true
+          shouldCompress = true;
         }
       }
       if (shouldCompress && body.length > 1024) {
-        const ae = this.headers.get('accept-encoding') || ''
-        if (ae.includes('br')) {
-          this.#resp.headers.set('Vary', 'Origin')
-          this.#resp.headers.set('Content-Encoding', 'br')
+        const ae = this.headers.get("accept-encoding") || ""
+        if (ae.includes("br")) {
+          this.#resp.headers.set("Vary", "Origin")
+          this.#resp.headers.set("Content-Encoding", "br");
           if (brotli === null) {
-            const { compress } = await import('https://deno.land/x/brotli@v0.1.4/mod.ts')
-            brotli = compress
+            const { compress } = await import(
+              "https://deno.land/x/brotli@v0.1.4/mod.ts"
+            )
+            brotli = compress;
           }
           body = brotli(body)
-        } else if (ae.includes('gzip')) {
-          this.#resp.headers.set('Vary', 'Origin')
-          this.#resp.headers.set('Content-Encoding', 'gzip')
+        } else if (ae.includes("gzip")) {
+          this.#resp.headers.set("Vary", "Origin")
+          this.#resp.headers.set("Content-Encoding", "gzip");
           if (gzip === null) {
-            const denoflate = await import('https://deno.land/x/denoflate@1.1/mod.ts')
-            gzip = (data: Uint8Array) => denoflate.gzip(data, undefined)
+            const denoflate = await import(
+              "https://deno.land/x/denoflate@1.1/mod.ts"
+            )
+            gzip = (data: Uint8Array) => denoflate.gzip(data, undefined);
           }
-          body = gzip(body)
+          body = gzip(body);
         }
       }
     }
-    this.#resp.headers.set('Date', (new Date).toUTCString())
-    this.#resp.done = true
+    this.#resp.headers.set("Date", (new Date()).toUTCString())
+    this.#resp.done = true;
     try {
       await this.respond({
         status: this.#resp.status,
         headers: this.#resp.headers,
-        body
-      })
+        body,
+      });
     } catch (err) {
-      log.warn('ServerRequest.respond:', err.message)
+      log.warn("ServerRequest.respond:", err.message);
     }
   }
 
-  json(data: any, replacer?: (this: any, key: string, value: any) => any, space?: string | number): Promise<void> {
-    return this.send(JSON.stringify(data, replacer, space), 'application/json; charset=utf-8')
+  json(
+    data: any,
+    replacer?: (this: any, key: string, value: any) => any,
+    space?: string | number,
+  ): Promise<void> {
+    return this.send(
+      JSON.stringify(data, replacer, space),
+      "application/json; charset=utf-8",
+    );
   }
 }
