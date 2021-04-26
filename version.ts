fccb0de6b2514e0ca6a03218b438e64fa56f20b1
X <i@jex.me>
2021-04-26 22:52:08 +0800

    Remove publish.ts

--- version.ts
+++ version.ts
@@ -1,1 +1,15 @@
+// version managed by https://deno.land/x/publish
+
 export const VERSION = '0.3.0-alpha.32'
+
+export async function prepublish(version: string) {
+  const p = Deno.run({
+    cmd: ['deno', 'run', '-A', 'build.ts'],
+    cwd: './compiler',
+    stdout: 'inherit',
+    stderr: 'inherit',
+  })
+  const { success } = await p.status()
+  p.close()
+  return success
+}
