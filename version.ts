f5a6abfc88b7d3249be62b22738bf4547a404e3f
X <i@jex.me>
2021-04-26 23:31:53 +0800

    Revert import maps

--- version.ts
+++ version.ts
@@ -1,5 +1,6 @@
-// version managed by https://deno.land/x/publish
+import { defaultReactVersion } from './shared/constants.ts'
 
+/** `VERSION` managed by https://deno.land/x/publish */
 export const VERSION = '0.3.0-alpha.32'
 
 export async function prepublish(version: string) {
@@ -11,5 +12,20 @@ export async function prepublish(version: string) {
   })
   const { success } = await p.status()
   p.close()
+  if (success) {
+    const data = await Deno.readTextFile('./import_map.json')
+    const importMap = JSON.parse(data)
+    Object.assign(importMap.imports, {
+      'aleph/': `https://deno.land/x/aleph@v${version}/`,
+      'framework': `https://deno.land/x/aleph@v${version}/framework/core/mod.ts`,
+      'framework/react': `https://deno.land/x/aleph@v${version}/framework/react/mod.ts`,
+      'react': `https://esm.sh/react@${defaultReactVersion}`,
+      'react-dom': `https://esm.sh/react-dom@${defaultReactVersion}`,
+    })
+    await Deno.writeTextFile(
+      './import_map.json',
+      JSON.stringify(importMap, undefined, 2)
+    )
+  }
   return success
 }
