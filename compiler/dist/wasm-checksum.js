032ef43c2924ac2eb87d7b7ad12b22697c7a0496
Niclas Overby <niclas@overby.me>
2021-05-15 08:53:23 +0200

    Build compiler wasm

--- compiler/dist/wasm-checksum.js
+++ compiler/dist/wasm-checksum.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const checksum = "7ae5ad874d024de2aa74d70607950875e8454ba5";
+=======
+export const checksum = "28cae3200e5875b6d03667a2dfe4c446ff29c15e";
+>>>>>>> Build compiler wasm
