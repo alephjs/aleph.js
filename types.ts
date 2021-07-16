35d502e3e05a38c1f3d6dbf6bbbf40f9f535b4b7
X <i@jex.me>
2021-07-16 10:05:59 +0800

    Refactor types

--- types.ts
+++ types.ts
@@ -1,35 +1,42 @@
+
+/**
+ * An interface that aligns to the parts of the `Aleph`.
+ */
+export interface Aleph {
+  readonly mode: 'development' | 'production'
+  readonly workingDir: string
+  readonly buildDir: string
+  readonly config: Required<Config>
+  readonly importMap: ImportMap
+  fetchModule(specifier: string): Promise<{ content: Uint8Array, contentType: string | null }>
+  addModule(specifier: string, sourceCode?: string): Promise<void>
+  addDist(path: string, content: Uint8Array): Promise<void>
+  injectCode(phase: 'compilation' | 'hmr' | 'ssr', test: RegExp | string, transform: (specifier: string, code: string, map?: string) => { code: string, map?: string }): void
+  injectCode(phase: 'compilation' | 'hmr' | 'ssr', transform: (specifier: string, code: string, map?: string) => { code: string, map?: string }): void
+}
+
 /**
  * The config for aleph server.
  */
 export type Config = {
   /** `framework` specifies the framework (default is 'react'). */
   framework?: 'react'
-  /** `buildTarget` specifies the build target in production mode (default is [**es2015**]). */
-  buildTarget?: BuildTarget
-  /** `browserslist` specifies the target browsers for esbuild. */
-  browserslist?: BrowsersList
-  /** `basePath` specifies the path prefix for the application (default is '/'). */
+  /** `basePath` specifies the path prefix (default is '/'). */
   basePath?: string
-  /** `srcDir` specifies the **src** dir (default is '/'). */
-  srcDir?: string
-  /** `outputDir` specifies the output directory for `build` command (default is '**dist**'). */
-  outputDir?: string
-  /** `defaultLocale` specifies the default locale of the application (default is '**en**'). */
+  /** `defaultLocale` specifies the default locale (default is '**en**'). */
   defaultLocale?: string
   /** `locales` specifies the available locales. */
   locales?: string[]
+  /** `build` specifies the options for **ES Build**. */
+  build?: BuildOptions
   /** `ssr` specifies the options for **SSR**. */
-  ssr?: boolean | GloablSSROptions
-  /** `plugins` specifies some plugins for the appliaction. */
-  plugins?: (LoaderPlugin | ServerPlugin)[]
+  ssr?: boolean | GlobalSSROptions
+  /** `server` specifies the options for **Server**. */
+  server?: ServerOptions
   /** `css` specifies the css processing options. */
   css?: CSSOptions
-  /** `headers` appends custom headers for server requests. */
-  headers?: Record<string, string>
-  /** `rewrites` specifies the server rewrite map. */
-  rewrites?: Record<string, string>
-  /** `compress` enbles gzip/brotli compression for static files and SSR content (default is **true**). */
-  compress?: boolean
+  /** `plugins` specifies some plugins to extend Aleph runtime. */
+  plugins?: (LoaderPlugin | ServerPlugin)[]
   /** `env` appends system env variables. */
   env?: Record<string, string>
 }
@@ -67,12 +74,7 @@ export type ServerPlugin = {
 }
 
 /**
- * The Plugin for postcss.
- */
-export type PostCSSPlugin = string | [string, any] | Record<string, any> | CallableFunction
-
-/**
- * The result of loader's resolve method.
+ * The result of loader plugin's `resolve` method.
  */
 export type ResolveResult = {
   specifier: string,
@@ -83,7 +85,7 @@ export type ResolveResult = {
 }
 
 /**
- * The output of loader's load method.
+ * The output of loader plugin's `load` method.
  */
 export type LoaderOutput = {
   /** The transformed code type (default is 'js'). */
@@ -95,20 +97,25 @@ export type LoaderOutput = {
 }
 
 /**
- * The built target for build phase.
+ * The built target for esbuild.
  */
 export type BuildTarget = 'es2015' | 'es2016' | 'es2017' | 'es2018' | 'es2019' | 'es2020' | 'esnext'
 
 /**
- * The borwser names for esbuild.
+ * The supported borwser names for esbuild.
  */
 export type BrowserNames = 'chrome' | 'edge' | 'firefox' | 'ios' | 'safari'
 
 /**
- * The borwser names list for esbuild.
+ * The config for ES Build.
  */
-export type BrowsersList = {
-  [key in BrowserNames]?: number
+export type BuildOptions = {
+  /** `target` specifies the build target in production mode (default is [**es2015**]). */
+  target?: BuildTarget
+  /** `browsers` specifies the target browsers for esbuild. */
+  browsers?: Record<BrowserNames, number>
+  /** `outputDir` specifies the output directory for `build` command (default is '**dist**'). */
+  outputDir?: string
 }
 
 /**
@@ -134,6 +141,11 @@ export type CSSOptions = {
 }
 
 /**
+ * The Plugin type for postcss.
+ */
+export type PostCSSPlugin = string | [string, any] | Record<string, any> | CallableFunction
+
+/**
  * The options are passed on to postcss-modules.
  */
 export type CSSModulesOptions = {
@@ -148,7 +160,7 @@ export type CSSModulesOptions = {
 /**
  * Global **SSR** options.
  */
-export type GloablSSROptions = {
+export type GlobalSSROptions = {
   /** A list of RegExp for paths to use **SSR**. */
   include?: RegExp[]
   /** A list of RegExp for paths to skip **SSR**. */
@@ -156,7 +168,7 @@ export type GloablSSROptions = {
 }
 
 /**
- * Page **SSR** options.
+ * The **SSR** options for pages.
  */
 export type SSROptions = {
   props?(router: RouterURL): ({ props: Record<string, any>, expires?: number } | Promise<{ props: Record<string, any>, expires?: number }>)
@@ -164,32 +176,44 @@ export type SSROptions = {
 }
 
 /**
- * An interface that aligns to the parts of the `Aleph`.
+ * The options for Server.
  */
-export interface Aleph {
-  readonly mode: 'development' | 'production'
-  readonly workingDir: string
-  readonly buildDir: string
-  readonly config: Required<Config>
-  readonly importMap: ImportMap
-  fetchModule(specifier: string): Promise<{ content: Uint8Array, contentType: string | null }>
-  addModule(specifier: string, sourceCode?: string): Promise<void>
-  addDist(path: string, content: Uint8Array): Promise<void>
-  injectCode(phase: 'compilation' | 'hmr' | 'ssr', test: RegExp | string, transform: (specifier: string, code: string, map?: string) => { code: string, map?: string }): void
-  injectCode(phase: 'compilation' | 'hmr' | 'ssr', transform: (specifier: string, code: string, map?: string) => { code: string, map?: string }): void
+export type ServerOptions = {
+  /** A list of `APIMiddleware` for api requests. */
+  middlewares?: APIMiddleware[]
+  /** `headers` appends custom headers for server requests. */
+  headers?: Record<string, string>
+  /** `rewrites` specifies the server rewrite map. */
+  rewrites?: Record<string, string>
+  /** `compress` enbles compression(gzip/brotli) for static files and SSR content (default is **true**). */
+  compress?: boolean
 }
 
 /**
- * An interface extends the `ServerRequest` for API requests.
+ * The middleware for api requests.
  */
-export interface APIRequest {
-  readonly req: Request
-  readonly router: RouterURL
+export type APIMiddleware = {
+  (req: APIRequest, next: () => void): void
+}
 
+/**
+ * The main param for API requests.
+ */
+export type APIRequest = {
+  readonly req: Request
+  readonly resp: APIResponse
+  readonly router: RouterURL
+  readonly data: Map<string, any>
+}
+
+/**
+ * An interface that aligns to the parts of the `Aleph`.
+ */
+export interface APIResponse {
   /**
-   * `addHeader` adds a new value onto an existing response header of the request, or
-   * adds the header if it does not already exist.
-   */
+  * `addHeader` adds a new value onto an existing response header of the request, or
+  * adds the header if it does not already exist.
+  */
   addHeader(key: string, value: string): this
   /**
    * `setHeader` sets a new value for an existing response header of the request, or adds
@@ -205,20 +229,11 @@ export interface APIRequest {
   /** `json` replies to the request with a json content. */
   json(data: any): Promise<void>
   /** `redirect` replies to redirect the client to another URL with optional response `status` defaulting to 302. */
-  redirect(url: string, status?: number): this
+  redirect(url: string, status?: number): Promise<void>
 }
 
 /**
- * A handler to handle api requests.
- *
- * @param req APIRequest object
- */
-export type APIHandler = {
-  (req: APIRequest): void
-}
-
-/**
- * The router url object of the routing, you can access it with `useRouter()` hook.
+ * The router URL object of the routing.
  */
 export type RouterURL = {
   readonly basePath: string
