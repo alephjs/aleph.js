b7918ad138329e36508f36e5685058c276dfa93d
X <i@jex.me>
2021-07-31 00:57:42 +0800

    Remove oak types dep

--- server/oak.ts
+++ server/oak.ts
@@ -1,21 +1,18 @@
-import type { Context } from 'https://deno.land/x/oak@v7.7.0/context.ts'
-import type { Middleware } from 'https://deno.land/x/oak@v7.7.0/middleware.ts'
-import { NativeRequest } from 'https://deno.land/x/oak@v7.7.0/http_server_native.ts'
-import { Aleph } from './aleph.ts'
+import type { Aleph } from './aleph.ts'
 import { Server } from './server.ts'
 
-/** `oakify` creates an **oak** middleware for Aleph app. */
-export function oakify(aleph: Aleph): Middleware {
+/** `oakify` creates an **oak** middleware with Aleph server. */
+export function oakify(aleph: Aleph) {
   const server = new Server(aleph)
 
-  return (ctx: Context) => {
+  return (ctx: any) => {
     const { originalRequest } = ctx.request
-    if (originalRequest instanceof NativeRequest) {
+    if ('respond' in originalRequest) {
       const { request, respond } = originalRequest
       server.handle({ request, respondWith: respond })
       ctx.respond = false
     } else {
-      ctx.throw(500, 'Aleph.js doesn\'t support oak `ServerRequest` yet')
+      ctx.throw(500, 'Aleph.js doesn\'t support std `ServerRequest` yet')
     }
   }
 }
