7621fddf13762eb070bd5d0f49606e19a7f3e145
X <i@jex.me>
2021-08-05 10:12:17 +0800

    Update examples

--- examples/42-wasm/wasm_loader.ts
+++ examples/42-wasm/wasm_loader.ts
@@ -1,21 +1,18 @@
 import type { Plugin } from 'aleph/types.ts'
 
-export default (): Plugin => ({
+export default <Plugin>{
   name: 'wasm-loader',
-  setup(aleph) {
-    aleph.addModuleLoader({
-      test: /\.wasm$/i,
-      load: async ({ specifier }, aleph) => {
-        const { content } = await aleph.fetchModule(specifier)
-        return {
-          code: [
-            `const wasmBytes = new Uint8Array([${content.join(',')}])`,
-            'const wasmModule = new WebAssembly.Module(wasmBytes)',
-            'const { exports } = new WebAssembly.Instance(wasmModule)',
-            'export default exports',
-          ].join('\n')
-        }
+  setup: aleph => {
+    aleph.onLoad(/\.wasm$/i, async ({ specifier }) => {
+      const { content } = await aleph.fetchModuleSource(specifier)
+      return {
+        code: [
+          `const wasmBytes = new Uint8Array([${content.join(',')}])`,
+          'const wasmModule = new WebAssembly.Module(wasmBytes)',
+          'const { exports } = new WebAssembly.Instance(wasmModule)',
+          'export default exports',
+        ].join('\n')
       }
     })
   }
-})
+}
