a3988e7b5f27ea9f1e4fb6edebc2882eb1a11933
X <i@jex.me>
2021-08-26 02:53:54 +0800

    Update import map

--- version.ts
+++ version.ts
@@ -1,5 +1,3 @@
-import { defaultReactVersion } from './shared/constants.ts'
-
 /** `VERSION` managed by https://deno.land/x/publish */
 export const VERSION = '0.3.0-beta.4'
 
@@ -13,21 +11,5 @@ export async function prepublish(version: string): Promise<boolean> {
   })
   const { success } = await p.status()
   p.close()
-  if (success) {
-    const data = await Deno.readTextFile('./import_map.json')
-    const importMap = JSON.parse(data)
-    Object.assign(importMap.imports, {
-      'aleph/': `https://deno.land/x/aleph@v${version}/`,
-      'aleph/types': `https://deno.land/x/aleph@v${version}/types.d.ts`,
-      'aleph/web': `https://deno.land/x/aleph@v${version}/framework/core/mod.ts`,
-      'aleph/react': `https://deno.land/x/aleph@v${version}/framework/react/mod.ts`,
-      'react': `https://esm.sh/react@${defaultReactVersion}`,
-      'react-dom': `https://esm.sh/react-dom@${defaultReactVersion}`,
-    })
-    await Deno.writeTextFile(
-      './import_map.json',
-      JSON.stringify(importMap, undefined, 2)
-    )
-  }
   return success
 }
