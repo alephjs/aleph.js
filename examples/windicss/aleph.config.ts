43d246a933f0a186b02696defc16bbb1cf386bf5
X <i@jex.me>
2021-09-02 23:10:33 +0800

    Rename tailwindcss to windicss

--- examples/windicss/aleph.config.ts
+++ examples/windicss/aleph.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'aleph/types'
-import windicss from 'https://deno.land/x/aleph_plugin_windicss@v0.0.1/plugin.ts'
+import windicss from 'https://deno.land/x/aleph_plugin_windicss@v0.0.2/plugin.ts'
 
 export default <Config>{
   plugins: [windicss]
