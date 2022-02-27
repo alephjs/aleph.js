03ef3fb9bbc44fff544cdbca98e4e36156d90fee
X <i@jex.me>
2022-02-28 00:46:15 +0800

    Update react-app example

--- examples/react-app/routes/index.tsx
+++ examples/react-app/routes/index.tsx
@@ -1,66 +1,49 @@
-import { Head, useData } from "aleph/react";
+import { Head, Link } from "aleph/react";
 
-let count = parseInt(window.localStorage?.getItem("count") || "0") || 0;
-
-export const data = {
-  get: (req: Request) => {
-    return new Response(JSON.stringify({ count }));
-  },
-  post: async (req: Request) => {
-    const { action } = await req.json();
-    if (action === "increase") {
-      count++;
-    } else if (action === "decrease") {
-      count--;
-    }
-    window.localStorage?.setItem("count", count.toString());
-    return new Response(JSON.stringify({ count }));
-  },
-};
+const linkIcon = (
+  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M14 5C13.4477 5 13 4.55228 13 4C13 3.44772 13.4477 3 14 3H20C20.2652 3 20.5196 3.10536 20.7071 3.29289C20.8946 3.48043 21 3.73478 21 4L21 10C21 10.5523 20.5523 11 20 11C19.4477 11 19 10.5523 19 10L19 6.41422L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L17.5858 5H14ZM3 7C3 5.89543 3.89543 5 5 5H10C10.5523 5 11 5.44772 11 6C11 6.55228 10.5523 7 10 7H5V19H17V14C17 13.4477 17.4477 13 18 13C18.5523 13 19 13.4477 19 14V19C19 20.1046 18.1046 21 17 21H5C3.89543 21 3 20.1046 3 19V7Z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 export default function Index() {
-  const { data, isLoading, isMutating, mutation } = useData<{ count: number }>();
-
   return (
-    <div className="page">
+    <div className="page y-center">
       <Head>
-        <title>Hello World - Aleph.js</title>
+        <title>About Aleph.js</title>
       </Head>
       <p className="logo">
         <img src="/assets/logo.svg" height="60" title="Aleph.js" />
       </p>
       <h1>
-        Welcome to use <strong>Aleph.js</strong>!
+        The Fullstack Framework in Deno.
       </h1>
-      <p className="links">
-        <a href="https://alephjs.org" target="_blank">Website</a>
-        <span></span>
+      <p>
+        <strong>Aleph.js</strong> gives you the best developer experience for building web applications<br />{" "}
+        with modern tooling.
+      </p>
+      <div className="links">
         <a href="https://alephjs.org/docs/get-started" target="_blank">
           Get Started
+          {linkIcon}
         </a>
-        <span></span>
-        <a href="https://alephjs.org/docs" target="_blank">Docs</a>
-        <span></span>
-        <a href="https://github.com/alephjs/aleph.js" target="_blank">Github</a>
-      </p>
-      <div className="counter">
-        <span>Counter:</span>
-        {isLoading && <em>...</em>}
-        {!isLoading && <strong>{data?.count}</strong>}
-        <button
-          disabled={Boolean(isMutating)}
-          onClick={() => mutation.post({ action: "decrease" }, "replace")}
-        >
-          -
-        </button>
-        <button
-          disabled={Boolean(isMutating)}
-          onClick={() => mutation.post({ action: "increase" }, "replace")}
-        >
-          +
-        </button>
+        <a href="https://alephjs.org/docs" target="_blank">
+          Docs
+          {linkIcon}
+        </a>
+        <a href="https://github.com/alephjs/aleph.js" target="_blank">
+          Github
+          {linkIcon}
+        </a>
       </div>
-      <p className="copyinfo">Built by Aleph.js in Deno</p>
+      <nav>
+        <Link to="/todos">
+          <button>Todos App Demo</button>
+        </Link>
+      </nav>
     </div>
   );
 }
---
+++ examples/react-app/routes/todos.tsx
@@ -0,0 +1,105 @@
+import { Head, Link, useData } from "aleph/react";
+
+type TodoItem = {
+  id: number;
+  message: string;
+  completed: boolean;
+};
+
+let todos: TodoItem[] = JSON.parse(window.localStorage?.getItem("todos") || "[]");
+
+export const data = {
+  get: (req: Request) => {
+    return new Response(JSON.stringify({ todos }));
+  },
+  put: async (req: Request) => {
+    const { message } = await req.json();
+    if (typeof message === "string") {
+      todos.push({ id: Date.now(), message, completed: false });
+      window.localStorage?.setItem("todos", JSON.stringify(todos));
+    }
+    return new Response(JSON.stringify({ todos }));
+  },
+  patch: async (req: Request) => {
+    const { id, message, completed } = await req.json();
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+      if (typeof message === "string") {
+        todo.message = message;
+      }
+      if (typeof completed === "boolean") {
+        todo.completed = completed;
+      }
+      window.localStorage?.setItem("todos", JSON.stringify(todos));
+    }
+    return new Response(JSON.stringify({ todos }));
+  },
+  delete: (req: Request) => {
+    const { searchParams } = new URL(req.url);
+    if (searchParams.has("id")) {
+      const id = parseInt(searchParams.get("id")!);
+      if (!Number.isNaN(id)) {
+        todos = todos.filter((todo) => todo.id !== id);
+        window.localStorage?.setItem("todos", JSON.stringify(todos));
+      }
+    }
+    return new Response(JSON.stringify({ todos }));
+  },
+};
+
+export default function Todos() {
+  const { data, isMutating, mutation } = useData<{ todos: TodoItem[] }>();
+
+  return (
+    <div className="page todos-app">
+      <Head>
+        <title>Todos App by Aleph.js</title>
+      </Head>
+      <h1>
+        <span>Todos</span>
+        {!!(data?.todos.length) && <em>{data?.todos.filter((todo) => todo.completed).length}/{data?.todos.length}</em>}
+      </h1>
+      <ul>
+        {data?.todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => mutation.patch({ id: todo.id, completed: !todo.completed }, "replace")}
+            />
+            <label className={todo.completed ? "completed" : ""}>{todo.message}</label>
+            {todo.id > 0 && <button onClick={() => mutation.delete({ id: todo.id.toString() }, "replace")}></button>}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const fd = new FormData(e.currentTarget);
+          const message = fd.get("message")?.toString().trim();
+          if (message) {
+            mutation.put({ message }, {
+              // optimistic update without waiting for the server response
+              optimisticUpdate: (data) => {
+                return {
+                  todos: [...data.todos, { id: 0, message, completed: false }],
+                };
+              },
+              // replace the data with the new data from the server
+              replace: true,
+            });
+            e.currentTarget.reset();
+          }
+        }}
+      >
+        <input
+          type="text"
+          name="message"
+          placeholder="What needs to be done?"
+          autoFocus
+          disabled={!!isMutating}
+        />
+      </form>
+    </div>
+  );
+}
