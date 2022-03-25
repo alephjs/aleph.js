d20b5344b4d538b9c5ddeb4f3078555e4b9cbcc4
X <i@jex.me>
2022-03-25 16:04:48 +0800

    Update types

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,9 @@
 declare type HTMLRewriterHandlers = {
-  element?: (element: import("https://deno.land/x/lol_html@0.0.2/types.d.ts").Element) => void;
-  text?: (text: import("https://deno.land/x/lol_html@0.0.2/types.d.ts").TextChunk) => void;
-  doctype?: (doctype: import("https://deno.land/x/lol_html@0.0.2/types.d.ts").Doctype) => void;
-  comments?: (comment: import("https://deno.land/x/lol_html@0.0.2/types.d.ts").Comment) => void;
-  end?: (end: import("https://deno.land/x/lol_html@0.0.2/types.d.ts").DocumentEnd) => void;
+  element?: (element: import("https://deno.land/x/lol_html@0.0.3/types.d.ts").Element) => void;
+  text?: (text: import("https://deno.land/x/lol_html@0.0.3/types.d.ts").TextChunk) => void;
+  doctype?: (doctype: import("https://deno.land/x/lol_html@0.0.3/types.d.ts").Doctype) => void;
+  comments?: (comment: import("https://deno.land/x/lol_html@0.0.3/types.d.ts").Comment) => void;
+  end?: (end: import("https://deno.land/x/lol_html@0.0.3/types.d.ts").DocumentEnd) => void;
 };
 
 declare interface FetchContext extends Record<string, unknown> {
