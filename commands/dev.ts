dff24476249c6ba82eca5069abdc55f45e69e873
X <i@jex.me>
2022-04-24 17:08:32 +0800

    Fix server restart loop in generate mode

--- commands/dev.ts
+++ commands/dev.ts
@@ -106,7 +106,7 @@ if (import.meta.main) {
         // emit HMR event
         if (e.all.has(`hotUpdate:${specifier}`)) {
           e.emit(`hotUpdate:${specifier}`, { specifier });
-        } else {
+        } else if (specifier !== "./route.gen.ts") {
           clientDependencyGraph?.lookup(specifier, (specifier) => {
             if (e.all.has(`hotUpdate:${specifier}`)) {
               e.emit(`hotUpdate:${specifier}`, { specifier });
