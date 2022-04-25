c2222aff8c49ddebd725a077e5e3169bb631f827
X <i@jex.me>
2022-04-25 18:20:23 +0800

    1.0.0-alpha.22

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "1.0.0-alpha.21";
+export const VERSION = "1.0.0-alpha.22";
 
 /** The flag indicates that the version is canary version. */
 export const isCanary = false;
