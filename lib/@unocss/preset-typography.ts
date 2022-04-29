21b93595dadb8474aa02c49f5b62438e62fc7bc0
X <i@jex.me>
2022-04-29 18:17:47 +0800

    Use unocss 0.32.1

--- lib/@unocss/preset-typography.ts
+++ lib/@unocss/preset-typography.ts
@@ -2,4 +2,4 @@
  * The typography preset.
  * https://github.com/unocss/unocss/tree/main/packages/preset-typography
  */
-export { default } from "https://esm.sh/@unocss/preset-typography@0.32.2";
+export { default } from "https://esm.sh/@unocss/preset-typography@0.32.1";
--- lib/@unocss/preset-uno.ts
+++ lib/@unocss/preset-uno.ts
@@ -1,1 +1,1 @@
-export { default } from "https://esm.sh/@unocss/preset-uno@0.32.2";
+export { default } from "https://esm.sh/@unocss/preset-uno@0.32.1";
