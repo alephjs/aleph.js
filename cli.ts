befffb9fe92e16108fabd4afc5ddf3ad2f88ff51
X <i@jex.me>
2022-05-01 16:49:10 +0800

    Upgrade esbuild to 0.14.38

--- cli.ts
+++ cli.ts
@@ -168,7 +168,7 @@ type RunOptions = {
 
 async function run(command: string, options: RunOptions) {
   const { version, isCanary, denoConfigFile, importMapFile, reload } = options;
-  const { esbuildBinDir, esbuildBinPath } = getEsbuildPath("0.14.36");
+  const { esbuildBinDir, esbuildBinPath } = getEsbuildPath("0.14.38");
   const devPort = Deno.env.get("ALEPH_DEV_PORT");
   const rwDirs = [
     Deno.env.get("MODULES_CACHE_DIR"),
