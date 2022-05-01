8707c2d94c162f592c683d0e68aa58226a6d0baf
X <i@jex.me>
2022-05-01 19:26:18 +0800

    1.0.0-alpha.29

--- version.ts
+++ version.ts
@@ -1,20 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "1.0.0-alpha.28";
+export const VERSION = "1.0.0-alpha.29";
 
 /** The flag indicates that the version is canary version. */
 export const isCanary = false;
-
-/** `prepublish` will be invoked before publish. */
-export async function prepublish(): Promise<boolean | void> {
-  if (window.confirm("Build compiler wasm?")) {
-    const p = Deno.run({
-      cmd: ["deno", "run", "-A", "build.ts"],
-      cwd: "./compiler",
-      stdout: "inherit",
-      stderr: "inherit",
-    });
-    const { success } = await p.status();
-    p.close();
-    return success;
-  }
-}
