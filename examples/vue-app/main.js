c96a934697e136f076002d440c45fcb2f160b22a
linbingquan <695601626@qq.com>
2022-05-11 13:16:02 +0800

    clean up.

--- examples/vue-app/main.js
+++ examples/vue-app/main.js
@@ -1,8 +1,8 @@
 import { App, createSSRApp } from "aleph/vue";
 import Header from "./components/Header.vue";
 
-const app = createSSRApp(App)
+const app = createSSRApp(App);
 
-app.component("Header", Header)
+app.component("Header", Header);
 
 app.mount("#root", true);
