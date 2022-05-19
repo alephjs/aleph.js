e355dc66cdd192a896b93d71ef809255bb6377e3
X <i@jex.me>
2022-05-19 20:00:30 +0800

    Fix yew app build

--- examples/yew-app/server.ts
+++ examples/yew-app/server.ts
@@ -1,42 +1,16 @@
 import { serve } from "aleph/server";
 import init, { ssr } from "./pkg/yew_app.js";
+import build from "./build.ts";
 
 await init(await Deno.readFile("./pkg/yew_app_bg.wasm"));
-
-async function buildWASM(forceBuild?: boolean) {
-  if (forceBuild || !existsFile("./pkg/package.json")) {
-    const p = Deno.run({
-      cmd: ["wasm-pack", "build", "--target", "web"],
-      stdout: "inherit",
-      stderr: "inherit",
-    });
-    await p.status();
-    p.close();
-  }
-}
-
-async function existsFile(path: string): Promise<boolean> {
-  try {
-    const stat = await Deno.lstat(path);
-    return stat.isFile;
-  } catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
-      return false;
-    }
-    throw err;
-  }
-}
 
 serve({
   devServer: {
     watchFS: (_kind, specifier) => {
       if (specifier.startsWith("./src/") && specifier.endsWith(".rs")) {
-        buildWASM(true);
+        build(true);
       }
     },
-  },
-  build: {
-    preBuild: buildWASM,
   },
   ssr: {
     render: (_ctx) => ssr(),
