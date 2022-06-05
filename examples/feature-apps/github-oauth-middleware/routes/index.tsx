df9f54e21c9710b71c9eeacbfbd3a8d6ea75514c
X <i@jex.me>
2022-06-05 16:42:56 +0800

    Update github oauth middleware demo style

--- examples/feature-apps/github-oauth-middleware/routes/index.tsx
+++ examples/feature-apps/github-oauth-middleware/routes/index.tsx
@@ -11,9 +11,9 @@ export default function Index() {
   const { data: { user } } = useData<{ user: GithubUser }>();
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="p-8 bg-gray-100 rounded-lg shadow-lg inline-flex items-center">
-        <img src={user.avatar_url} className="w-8 h-8" />
-        <strong>{user.name}</strong>
+      <div className="px-8 py-4 border border-gray-200 rounded-xl shadow-2xl inline-flex items-center gap-2">
+        <img src={user.avatar_url} className="w-7 h-7 rounded-full" />
+        <strong className="text-2xl font-bold">{user.name}</strong>
       </div>
     </div>
   );
