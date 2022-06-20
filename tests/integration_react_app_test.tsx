ec2cc406fcd9d580fc1226a55ea1c0cfdec2d64b
X <i@jex.me>
2022-06-20 13:20:15 +0800

    Update integration testing

--- tests/integration_react_app_test.tsx
+++ tests/integration_react_app_test.tsx
@@ -28,14 +28,25 @@ Deno.test("[integration] examples/react-app", async (t) => {
     assert(html.includes(`<script id="ssr-modules" type="application/json">`));
   });
 
-  await t.step("API GET /todos", async () => {
-    const res = await api.fetch("/todos");
-    const html = await res.text();
+  await t.step("API PUT+PATCH /todos", async () => {
+    const res = await api.fetch("/todos", { method: "PUT", body: JSON.stringify({ message: "better call saul!" }) });
+    const data = await res.json();
     assertEquals(res.status, 200);
-    assertEquals(res.headers.get("Content-Type"), "text/html; charset=utf-8");
-    assert(html.includes(`<title ssr>Todos</title>`));
-    assert(html.includes(`<h1><span>Todos</span></h1>`));
-    assert(html.includes(`<header style=`));
+    assertEquals(res.headers.get("Content-Type"), "application/json");
+    assertEquals(data.todos.length, 1);
+    assertEquals(data.todos.at(0).message, "better call saul!");
+    assertEquals(data.todos.at(0).completed, false);
+
+    const res2 = await api.fetch("/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id: data.todos.at(0).id, message: "Better Call Saul!", completed: true }),
+    });
+    const data2 = await res2.json();
+    assertEquals(res2.status, 200);
+    assertEquals(res2.headers.get("Content-Type"), "application/json");
+    assertEquals(data2.todos.length, 1);
+    assertEquals(data2.todos.at(0).message, "Better Call Saul!");
+    assertEquals(data2.todos.at(0).completed, true);
   });
 
   await t.step("API GET /todos?_data_", async () => {
@@ -43,7 +54,35 @@ Deno.test("[integration] examples/react-app", async (t) => {
     const data = await res.json();
     assertEquals(res.status, 200);
     assertEquals(res.headers.get("Content-Type"), "application/json");
-    assertEquals(data, { todos: [] });
+    assertEquals(data.todos.length, 1);
+  });
+
+  await t.step("API GET /todos", async () => {
+    const res = await api.fetch("/todos");
+    const html = await res.text();
+    assertEquals(res.status, 200);
+    assertEquals(res.headers.get("Content-Type"), "text/html; charset=utf-8");
+    assert(html.includes(`<title ssr>Todos</title>`));
+    assert(html.includes(`<header style=`));
+    assert(html.includes(`<div class="todos-app"><h1><span>Todos</span><em>1`));
+    assert(html.includes(`<label class="completed">Better Call Saul!</label>`));
+  });
+
+  await t.step("API DELETE /todos", async () => {
+    const res = await api.fetch("/todos?_data_");
+    const data = await res.json();
+    assertEquals(res.status, 200);
+    assertEquals(res.headers.get("Content-Type"), "application/json");
+    assertEquals(data.todos.length, 1);
+
+    const res2 = await api.fetch("/todos", {
+      method: "DELETE",
+      body: JSON.stringify({ id: data.todos.at(0).id }),
+    });
+    const data2 = await res2.json();
+    assertEquals(res2.status, 200);
+    assertEquals(res2.headers.get("Content-Type"), "application/json");
+    assertEquals(data2, { todos: [] });
   });
 
   await t.step("API GET /404", async () => {
