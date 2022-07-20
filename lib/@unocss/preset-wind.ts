d0590d2a7ed14c5124c91c66b4854d1af266cc4e
X <i@jex.me>
2022-07-20 18:05:28 +0800

    Upgrade unocss to 0.44.5

--- lib/@unocss/preset-wind.ts
+++ lib/@unocss/preset-wind.ts
@@ -2,4 +2,4 @@
  * Tailwind / Windi CSS compact preset.
  * https://github.com/unocss/unocss/tree/main/packages/preset-wind
  */
-export { default } from "https://esm.sh/@unocss/preset-wind@0.43.2";
+export { default } from "https://esm.sh/@unocss/preset-wind@0.44.5";
