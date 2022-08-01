e6033d09c2954c067c51ec40868ea530a0020ae4
X <i@jex.me>
2022-08-01 16:15:41 +0800

    Upgrade unocss to 0.45.1

--- lib/@unocss/preset-icons.ts
+++ lib/@unocss/preset-icons.ts
@@ -2,4 +2,4 @@
  * Use any icon as a class utility.
  * https://github.com/unocss/unocss/tree/main/packages/preset-icons
  */
-export { default } from "https://esm.sh/@unocss/preset-icons@0.44.5?no-require";
+export { default } from "https://esm.sh/@unocss/preset-icons@0.45.1?ignore-require";
--- lib/@unocss/preset-web-fonts.ts
+++ lib/@unocss/preset-web-fonts.ts
@@ -2,4 +2,4 @@
  * Web fonts at ease.
  * https://github.com/unocss/unocss/tree/main/packages/preset-web-fonts
  */
-export { default } from "https://esm.sh/@unocss/preset-web-fonts@0.44.5";
+export { default } from "https://esm.sh/@unocss/preset-web-fonts@0.45.1";
