c02789c86c5f6954102e1e5bf99e1985abc7539d
X <i@jex.me>
2022-09-06 01:57:01 +0800

    Update deps

--- server/deps.ts
+++ server/deps.ts
@@ -17,13 +17,13 @@ export {
 
 // third-party
 // @deno-types="https://deno.land/x/esbuild@v0.14.49/mod.d.ts"
-export { build as esbuild, type BuildResult, stop as stopEsbuild } from "https://deno.land/x/esbuild@v0.14.49/mod.js";
-export * from "https://deno.land/x/aleph_compiler@0.7.4/mod.ts";
-export * from "https://deno.land/x/aleph_compiler@0.7.4/types.ts";
+export { build as esbuild, type BuildResult, stop as stopEsbuild } from "https://deno.land/x/esbuild@v0.15.7/mod.js";
+export * from "https://deno.land/x/aleph_compiler@0.7.6/mod.ts";
+export * from "https://deno.land/x/aleph_compiler@0.7.6/types.ts";
 export { default as initLolHtml, HTMLRewriter } from "https://deno.land/x/lol_html@0.0.4/mod.js";
 export { default as lolHtmlWasm } from "https://deno.land/x/lol_html@0.0.4/wasm.js";
 export { JSONC } from "https://deno.land/x/jsonc_parser@v0.0.1/src/jsonc.ts";
 
 // npm
 export { default as mitt, type Emitter } from "https://esm.sh/mitt@3.0.0";
-export { default as MagicString } from "https://esm.sh/magic-string@0.26.2?target=esnext";
+export { default as MagicString } from "https://esm.sh/magic-string@0.26.3?target=esnext";
