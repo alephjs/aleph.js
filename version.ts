2b2bb5a9e354df38f123b9f92576f82499a6c2e7
X <i@jex.me>
2022-09-20 03:03:00 +0800

    1.0.0-beta.9

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "1.0.0-beta.8";
+export const VERSION = "1.0.0-beta.9";
 
 /** The flag indicates that the version is canary version. */
 export const isCanary = false;
