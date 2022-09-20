c6a82ee492d40ea6ca10f9881e21e90a54e1d433
X <i@jex.me>
2022-09-21 04:16:33 +0800

    Add mdx optioins for mdx-loader

--- runtime/react/mdx-loader.ts
+++ runtime/react/mdx-loader.ts
@@ -1,17 +1,27 @@
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "../../server/types.ts";
 import { compile } from "https://esm.sh/@mdx-js/mdx@2.1.3?no-dts";
-import remarkFrontmatter from "https://esm.sh/remark-frontmatter@4.0.1?no-dts";
-import remarkGfm from "https://esm.sh/remark-gfm@3.0.1?no-dts";
+
+export type Options = {
+  jsxImportSource?: string;
+  remarkPlugins?: unknown[];
+  rehypePlugins?: unknown[];
+};
 
 export default class SolidTransformer implements ModuleLoader {
+  #options: Options;
+
+  constructor(options?: Options) {
+    this.#options = options ?? {};
+  }
+
   test(path: string): boolean {
     return path.endsWith(".mdx") || path.endsWith(".md") || path.endsWith(".markdown");
   }
 
   async load(_specifier: string, content: string, env: ModuleLoaderEnv): Promise<ModuleLoaderOutput> {
     const ret = await compile(content, {
-      jsxImportSource: env.importMap?.imports["react"] ?? "https://esm.sh/react@18",
-      remarkPlugins: [remarkFrontmatter, remarkGfm],
+      jsxImportSource: env.jsxConfig?.jsxImportSource ?? "https://esm.sh/react@18",
+      ...this.#options,
     });
     return {
       code: ret.toString(),
