9d796d4a51e1e1131320b5aaad3b756dbaf7639b
X <i@jex.me>
2022-09-20 23:59:26 +0800

    Add frontmatter and gfm remark plugins for mdx

--- runtime/react/mdx-loader.ts
+++ runtime/react/mdx-loader.ts
@@ -1,5 +1,7 @@
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "../../server/types.ts";
 import { compile } from "https://esm.sh/@mdx-js/mdx@2.1.3?no-dts";
+import remarkFrontmatter from "https://esm.sh/remark-frontmatter@4.0.1?no-dts";
+import remarkGfm from "https://esm.sh/remark-gfm@3.0.1?no-dts";
 
 export default class SolidTransformer implements ModuleLoader {
   test(path: string): boolean {
@@ -9,6 +11,7 @@ export default class SolidTransformer implements ModuleLoader {
   async load(_specifier: string, content: string, env: ModuleLoaderEnv): Promise<ModuleLoaderOutput> {
     const ret = await compile(content, {
       jsxImportSource: env.importMap?.imports["react"] ?? "https://esm.sh/react@18",
+      remarkPlugins: [remarkFrontmatter, remarkGfm],
     });
     return {
       code: ret.toString(),
