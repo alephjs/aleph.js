186e858f0dfd54963f967c8b3184da7b541c5705
X <i@jex.me>
2022-10-01 16:12:36 +0800

    Update mdx loader options

--- runtime/react/mdx-loader.ts
+++ runtime/react/mdx-loader.ts
@@ -1,21 +1,10 @@
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "../../server/types.ts";
-import { compile } from "https://esm.sh/@mdx-js/mdx@2.1.3?no-dts";
-
-export type Options = {
-  /** Place to import automatic JSX runtimes from (default use the option in `deno.json`). */
-  jsxImportSource?: string;
-  /** Options to pass through to remark-rehype. The option allowDangerousHtml will always be set to true and the MDX nodes are passed through. */
-  remarkPlugins?: unknown[];
-  /** List of remark plugins, presets, and pairs. */
-  rehypePlugins?: unknown[];
-  /** List of recma plugins. This is a new ecosystem, currently in beta, to transform esast trees (JavaScript). */
-  recmaPlugins?: unknown[];
-};
+import { compile, type CompileOptions } from "https://esm.sh/@mdx-js/mdx@2.1.3";
 
 export default class MDXLoader implements ModuleLoader {
-  #options: Options;
+  #options: CompileOptions;
 
-  constructor(options?: Options) {
+  constructor(options?: CompileOptions) {
     this.#options = options ?? {};
   }
 
@@ -28,8 +17,11 @@ export default class MDXLoader implements ModuleLoader {
       { path: specifier, value: content },
       {
         jsxImportSource: this.#options.jsxImportSource ?? env.jsxConfig?.jsxImportSource ?? "https://esm.sh/react@18",
-        development: env.isDev,
         ...this.#options,
+        providerImportSource: this.#options.providerImportSource
+          ? env.importMap?.imports[this.#options.providerImportSource] ?? this.#options.providerImportSource
+          : undefined,
+        development: env.isDev,
       },
     );
     return {
