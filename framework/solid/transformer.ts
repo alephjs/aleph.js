145e3495bc254be5a181736df9cd3fd09e33c2ce
X <i@jex.me>
2023-03-25 23:24:27 +0800

    Re-enable solid refresh

--- framework/solid/transformer.ts
+++ framework/solid/transformer.ts
@@ -1,6 +1,6 @@
-import { transform } from "https://esm.sh/v110/@babel/standalone@7.21.3";
-import babelPresetSolid from "https://esm.sh/v110/babel-preset-solid@1.6.12";
-// import solidRefresh from "https://esm.sh/v110/solid-refresh@0.5.1/babel";
+import { transform } from "https://esm.sh/v112/@babel/standalone@7.21.3";
+import babelPresetSolid from "https://esm.sh/v112/babel-preset-solid@1.6.12";
+import solidRefresh from "https://esm.sh/v112/solid-refresh@0.5.2/babel";
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "../../server/types.ts";
 
 export default class SolidTransformer implements ModuleLoader {
@@ -9,12 +9,13 @@ export default class SolidTransformer implements ModuleLoader {
   }
 
   load(specifier: string, content: string, env: ModuleLoaderEnv): ModuleLoaderOutput {
+    console.log("transforming", specifier, content);
     const ret = transform(content, {
       presets: [
         [babelPresetSolid, { generate: env.ssr ? "ssr" : "dom", hydratable: env.ssr || !env.spaMode }],
         ["typescript", { isTSX: true, allExtensions: true }],
       ],
-      // plugins: env.isDev && !env.ssr ? [[solidRefresh, { bundler: "vite" }]] : [],
+      plugins: env.isDev && !env.ssr ? [[solidRefresh, { bundler: "vite" }]] : [],
       filename: specifier,
     });
     if (!ret) {
