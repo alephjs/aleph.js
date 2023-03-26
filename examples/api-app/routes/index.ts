e167ccc6690a3dec96b52c1a9f1956a2c5095f0f
X <i@jex.me>
2023-03-26 20:20:32 +0800

    Update api example

--- examples/api-app/routes/index.ts
+++ examples/api-app/routes/index.ts
@@ -1,6 +1,7 @@
 // GET "/"
-export function GET(req: Request) {
+export function GET(req: Request, ctx: Context) {
   const url = new URL(req.url);
+  console.log("[middleware:foo]", ctx.foo);
   return Response.json({
     "users_url": `${url.origin}/users`,
     "user_url": `${url.origin}/users/{user}`,
--- examples/api-app/server.ts
+++ examples/api-app/server.ts
@@ -2,11 +2,23 @@ import { serve } from "aleph/server";
 import denoDeploy from "aleph/plugins/deploy";
 import modules from "./routes/_export.ts";
 
+declare global {
+  interface Context {
+    foo: string;
+  }
+}
+
 serve({
   plugins: [
     denoDeploy({ modules }),
   ],
   middlewares: [
-    // add your middlewares here
+    {
+      name: "foo",
+      fetch: (_req, ctx) => {
+        ctx.foo = "bar";
+        return ctx.next();
+      },
+    },
   ],
 });
