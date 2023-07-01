a2e4a4edb9de2ff28232520ef36b2db1295ad2a1
X <i@jex.me>
2023-07-01 22:09:15 +0800

    1.0.0-beta.44

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "1.0.0-beta.43";
+export const VERSION = "1.0.0-beta.44";
 
 /** The flag indicates that the version is canary version. */
 export const isCanary = false;
